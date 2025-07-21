b4477c9d2a6df94007b2d33ec01b26f4e89234bc
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-21 21:25:29 +0300

    feature: enhance home screen UI

---
+++ ui/src/main/java/com/example/ui/screens/home/sections/AnimatedSectionVisibility.kt
@@ -0,0 +1,22 @@
+package com.example.ui.screens.home.sections
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AnimatedSectionVisibility(visible: Boolean, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+        AnimatedVisibility(
+            modifier = modifier,
+            visible = visible,
+            enter = fadeIn(tween(700)),
+            exit = fadeOut(tween(700))
+
+        ) {
+            content()
+        }
+
+}
