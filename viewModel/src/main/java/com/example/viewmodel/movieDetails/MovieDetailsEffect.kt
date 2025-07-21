367240e087b5704fff73e0ff3a4529402365297b
Mona <monaayman584@gmail.com>
2025-07-21 03:49:31 +0300

    feature: add function to MovieDetailsEffect.kt to handle dialog effect

--- viewModel/src/main/java/com/example/viewmodel/movieDetails/MovieDetailsEffect.kt
+++ viewModel/src/main/java/com/example/viewmodel/movieDetails/MovieDetailsEffect.kt
@@ -3,4 +3,5 @@ package com.example.viewmodel.movieDetails
 sealed interface MovieDetailsEffect {
     object NavigateBackEffect : MovieDetailsEffect
     object NavigateToCastsScreenEffect : MovieDetailsEffect
+    object NavigateToLoginScreenEffect : MovieDetailsEffect
 }
