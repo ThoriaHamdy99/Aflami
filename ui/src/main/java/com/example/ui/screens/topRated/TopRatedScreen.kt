f912491691f8eea5354bb3af5c01c79b766c0cbf
noor serry <noor.serry.94@gmail.com>
2025-07-22 15:23:01 +0300

    refactor: simplify loading and error states in TopRatedScreen
    This commit refactors the `TopRatedScreen` to simplify the UI for loading and network error states.
    
    - The `Box` wrappers for `LoadingContainer` and `NoNetworkContainer` have been removed, and their modifiers are now directly applied to the respective containers.
    - The `zIndex` for `NoNetworkContainer` has been removed as it's not necessary when only one of the error/loading states is visible at a time due to `AnimatedSectionVisibility`.

--- ui/src/main/java/com/example/ui/screens/topRated/TopRatedScreen.kt
+++ ui/src/main/java/com/example/ui/screens/topRated/TopRatedScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -99,25 +98,15 @@ fun TopRatedContent(
             AnimatedSectionVisibility(
                 visible = state.isLoading
             ) {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    LoadingContainer(modifier = Modifier.zIndex(10f))
-                }
+                    LoadingContainer(modifier = Modifier.fillMaxSize().zIndex(10f))
             }
 
             AnimatedSectionVisibility(
                 visible = state.error == TopRatedUiState.TopRatedError.NetworkError
             ) {
-                Box(
-                    modifier = Modifier.fillMaxSize().zIndex(10f),
-                    contentAlignment = Alignment.Center
-                ) {
                     NoNetworkContainer(
                         onClickRetry = interactionListener::onClickRetryLoading
                     )
-                }
             }
 
             AnimatedSectionVisibility(
