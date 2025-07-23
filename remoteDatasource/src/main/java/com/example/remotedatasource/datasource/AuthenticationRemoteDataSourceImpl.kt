2947e83d022222931c8fa166affae73181bc5b83
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-23 18:16:47 +0300

    refactor: remove redundant log statements

--- remoteDatasource/src/main/java/com/example/remotedatasource/datasource/AuthenticationRemoteDataSourceImpl.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/datasource/AuthenticationRemoteDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.example.remotedatasource.datasource
 
-import android.util.Log
 import com.example.remotedatasource.serviceProvider.AuthenticationServiceProvider
 import com.example.repository.datasource.remote.AuthenticationRemoteSource
 
@@ -11,12 +10,9 @@ class AuthenticationRemoteDataSourceImpl(
         username: String,
         password: String,
     ): String {
-        Log.d("TAG", "loginWithPassword: CreateRequestToken")
         val unactiveToken = authenticationServiceProvider.createRequestToken()
-        Log.d("TAG", "loginWithPassword: createSessionWithLogin with $unactiveToken")
         val activeToken =
             authenticationServiceProvider.createSessionWithLogin(username, password, unactiveToken.requestToken ?: "")
-        Log.d("TAG", "loginWithPassword: createSession $activeToken")
         return authenticationServiceProvider.createSession(activeToken.requestToken ?: "").sessionId
     }
 }
