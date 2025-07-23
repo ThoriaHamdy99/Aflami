ad03ab3df4ee1ba64864cbda4e9c2f9319970973
mohamed <hassanmmd4@gmail.com>
2025-07-23 22:47:17 +0300

    refactor: change name

--- repository/src/main/java/com/example/repository/repository/AuthenticationRepositoryImpl.kt
+++ repository/src/main/java/com/example/repository/repository/AuthenticationRepositoryImpl.kt
@@ -3,21 +3,21 @@ package com.example.repository.repository
 import com.example.domain.repository.AuthenticationRepository
 import com.example.domain.utils.SessionType
 import com.example.repository.datasource.local.AuthenticationLocalSource
-import com.example.repository.mapper.local.UserLoginTypeMapper
+import com.example.repository.mapper.local.SessionTypeMapper
 
 class AuthenticationRepositoryImpl(
     private val authenticationLocalSource: AuthenticationLocalSource,
-    private val userLoginTypeMapper: UserLoginTypeMapper
+    private val sessionTypeMapper: SessionTypeMapper
 ): AuthenticationRepository {
     override suspend fun loginWithPassword(username: String, password: String) {
 
     }
 
     override suspend fun setSessionType(sessionType: SessionType) {
-        authenticationLocalSource.setSessionType(userLoginTypeMapper.toLocalLoginType(sessionType))
+        authenticationLocalSource.setSessionType(sessionTypeMapper.toLocalSessionType(sessionType))
     }
 
     override suspend fun getSessionType(): SessionType {
-        return userLoginTypeMapper.fromLocalLoginType(authenticationLocalSource.getSessionType())
+        return sessionTypeMapper.fromLocalSessionType(authenticationLocalSource.getSessionType())
     }
 }
