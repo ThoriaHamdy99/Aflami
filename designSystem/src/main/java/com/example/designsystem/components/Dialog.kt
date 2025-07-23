dc51a9bcd6ac6dcf9bf6b9564e93427f2affffa6
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-24 00:13:17 +0300

    feature: update dialog in design system.

--- designSystem/src/main/java/com/example/designsystem/components/Dialog.kt
+++ designSystem/src/main/java/com/example/designsystem/components/Dialog.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,7 +17,10 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.core.graphics.toColor
+import com.example.designsystem.theme.AflamiTheme
 import com.example.designsystem.theme.AppTheme
+import com.example.designsystem.utils.ThemeAndLocalePreviews
 
 @SuppressLint("ContextCastToActivity")
 @Composable
@@ -26,17 +33,37 @@ fun Dialog(
 ) {
     val activity: Activity = LocalContext.current as Activity
 
-    activity.window.navigationBarColor = behindDialogColor.toArgb()
-    activity.window.statusBarColor = behindDialogColor.toArgb()
+    val statusBarColor by remember {
+        mutableIntStateOf(
+            activity.window.statusBarColor.toColor().toArgb()
+        )
+    }
+    val navigationBarColor by remember {
+        mutableIntStateOf(
+            activity.window.navigationBarColor.toColor().toArgb()
+        )
+    }
+
+    DisposableEffect(key1 = statusBarColor) {
+        onDispose {
+            activity.window.statusBarColor = statusBarColor
+            activity.window.navigationBarColor = navigationBarColor
+        }
+    }
 
     Dialog(
-        onDismissRequest = onDismiss,
+        onDismissRequest = {
+            onDismiss()
+        },
         properties = DialogProperties(
             usePlatformDefaultWidth = false,
             dismissOnBackPress = isDismissible,
-            dismissOnClickOutside = isDismissible
+            dismissOnClickOutside = isDismissible,
+
         ),
     ) {
+        activity.window.navigationBarColor = behindDialogColor.toArgb()
+        activity.window.statusBarColor = behindDialogColor.toArgb()
         Box(
             modifier = modifier
                 .fillMaxSize()
@@ -45,5 +72,18 @@ fun Dialog(
         ) {
             content()
         }
+    }
+}
+
+@Composable
+@ThemeAndLocalePreviews
+fun DialogPreview() {
+    AflamiTheme {
+        Dialog(
+            content = {},
+            onDismiss = {},
+            isDismissible = true,
+            behindDialogColor = AppTheme.color.dialogBackground
+        )
     }
 }
