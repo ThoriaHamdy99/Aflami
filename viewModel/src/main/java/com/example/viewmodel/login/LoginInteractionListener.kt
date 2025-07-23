755053cc384a11f25b496ea240d4a5675b1b9a26
mohamed <hassanmmd4@gmail.com>
2025-07-23 14:59:01 +0200

    feature: login

--- viewModel/src/main/java/com/example/viewmodel/login/LoginInteractionListener.kt
+++ viewModel/src/main/java/com/example/viewmodel/login/LoginInteractionListener.kt
@@ -3,6 +3,8 @@ package com.example.viewmodel.login
 interface LoginInteractionListener {
     fun onUserNameUpdated(username: String)
     fun onPasswordUpdate(password: String)
+    fun onSetUserNameError(usernameError: String)
+    fun onSetPasswordError(passwordError: String)
     fun onShowPasswordClicked()
     fun onLoginClicked()
     fun onContinueAsGuestClicked()
