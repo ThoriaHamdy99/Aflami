19776087206db995baa71351e81dc290846bac6b
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-24 01:28:03 +0300

    refactor: integrate `loginWithPassword` with remote and local dataSource

--- repository/src/main/java/com/example/repository/repository/AuthenticationRepositoryImpl.kt
+++ repository/src/main/java/com/example/repository/repository/AuthenticationRepositoryImpl.kt
@@ -3,14 +3,20 @@ package com.example.repository.repository
 import com.example.domain.repository.AuthenticationRepository
 import com.example.domain.utils.SessionType
 import com.example.repository.datasource.local.AuthenticationLocalSource
+import com.example.repository.datasource.remote.AuthenticationRemoteSource
 import com.example.repository.mapper.local.SessionTypeMapper
 
 class AuthenticationRepositoryImpl(
+    private val authenticationRemoteSource: AuthenticationRemoteSource,
     private val authenticationLocalSource: AuthenticationLocalSource,
     private val sessionTypeMapper: SessionTypeMapper
-): AuthenticationRepository {
-    override suspend fun loginWithPassword(username: String, password: String) {
-
+) : AuthenticationRepository {
+    override suspend fun loginWithPassword(
+        username: String,
+        password: String,
+    ) {
+        val sessionId = authenticationRemoteSource.loginWithPassword(username, password)
+        authenticationLocalSource.cacheSessionId(sessionId)
     }
 
     override suspend fun setSessionType(sessionType: SessionType) {
