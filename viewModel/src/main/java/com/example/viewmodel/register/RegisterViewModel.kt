31c3a0cc4f6bbb3dbadd114fa54a112ea5f2cf98
mohamed <hassanmmd4@gmail.com>
2025-07-25 02:46:13 +0300

    fix: fix register url

--- viewModel/src/main/java/com/example/viewmodel/register/RegisterViewModel.kt
+++ viewModel/src/main/java/com/example/viewmodel/register/RegisterViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.viewmodel.register
 
+import com.example.viewmodel.BuildConfig
 import com.example.viewmodel.shared.BaseViewModel
 import com.example.viewmodel.utils.dispatcher.DispatcherProvider
 
 
 class RegisterViewModel(dispatcherProvider: DispatcherProvider) :
     BaseViewModel<RegisterUiState, RegisterEffect>(RegisterUiState(), dispatcherProvider) {
+    init {
+        updateState { it.copy(signUpUrl = BuildConfig.MOVIE_SIGN_UP_URL) }
+
+    }
 
     fun setLoading(isLoading: Boolean) {
         updateState { it.copy(isLoading = isLoading) }
