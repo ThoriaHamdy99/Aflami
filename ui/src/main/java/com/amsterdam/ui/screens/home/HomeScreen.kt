84eb1773f918dc2de768160d122a5d757b6881d2
Ahmed Mahgoub <aed7862@gmail.com>
2025-07-28 19:46:03 +0300

    refactor: fix overlapping of top bar with popular section

--- ui/src/main/java/com/amsterdam/ui/screens/home/HomeScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/home/HomeScreen.kt
@@ -7,27 +7,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.amsterdam.designsystem.theme.AflamiTheme
 import com.amsterdam.designsystem.theme.AppTheme
@@ -41,7 +33,6 @@ import com.amsterdam.ui.navigation.Route
 import com.amsterdam.ui.navigation.Route.MovieDetails
 import com.amsterdam.ui.screens.home.component.MovieMoodPickerDialogDialog
 import com.amsterdam.ui.screens.home.sections.AnimatedSectionVisibility
-import com.amsterdam.ui.screens.home.sections.BlurredMoviePoster
 import com.amsterdam.ui.screens.home.sections.MoodPickerSection
 import com.amsterdam.ui.screens.home.sections.continueWatchingSection
 import com.amsterdam.ui.screens.home.sections.popularSection
@@ -57,7 +48,6 @@ import com.amsterdam.viewmodel.home.HomeViewModel
 import com.amsterdam.viewmodel.shared.uiStates.media.MediaType
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
-import kotlin.math.roundToInt
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, homeViewModel: HomeViewModel = koinViewModel()) {
@@ -103,64 +93,33 @@ private fun HomeScreenContent(
     modifier: Modifier = Modifier
 ) {
     val lazyListState = rememberLazyListState()
-    val pagerState = rememberPagerState(initialPage = Int.MAX_VALUE / 2) { Int.MAX_VALUE }
+
 
     val scrollOffset = remember {
         derivedStateOf { lazyListState.firstVisibleItemScrollOffset }
     }
 
     val appBarColor by animateColorAsState(
-        targetValue = if (scrollOffset.value > 10) AppTheme.color.surface else Color.Transparent,
+        targetValue = if (scrollOffset.value > 8) AppTheme.color.surface else Color.Transparent,
         animationSpec = tween(800),
         label = "AppBarScrollColor"
     )
-    var blurOffsetY by remember { mutableFloatStateOf(-12f) }
 
-    val nestedScrollConnection = remember {
-        object : NestedScrollConnection {
-            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                val newOffset = blurOffsetY + available.y
-                blurOffsetY = newOffset.coerceIn(minimumValue = -1000f, maximumValue = -12f)
-                return Offset.Zero
-            }
-        }
-    }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .nestedScroll(nestedScrollConnection)
             .navigationBarsPadding()
     ) {
-        AnimatedSectionVisibility(
-            visible = state.popularMediaSectionUiState.mediaItems.isNotEmpty() && remember { derivedStateOf { lazyListState.firstVisibleItemIndex } }.value == 0
-        ) {
-            BlurredMoviePoster(
-                posterUrl = state.popularMediaSectionUiState.mediaItems[pagerState.currentPage % state.popularMediaSectionUiState.mediaItems.size].posterUrl,
-                modifier = Modifier.offset { IntOffset(x = 0, y = blurOffsetY.roundToInt()) }
-            )
-        }
-
         LazyColumn(
             modifier = modifier.fillMaxSize(),
             contentPadding = PaddingValues(bottom = 100.dp),
             state = lazyListState,
         ) {
-            stickyHeader {
-                HomeAppBar(
-                    modifier = Modifier
-                        .background(appBarColor)
-                        .statusBarsPadding()
-                        .padding(horizontal = 16.dp, vertical = 8.dp),
-                    onSearchClicked = interactionListener::onClickSearch,
-                )
-            }
-
             popularSection(
                 state = state.popularMediaSectionUiState,
-                pagerState = pagerState,
                 onClickMediaItem = interactionListener::onClickMediaItem,
-                isVisible = state.error == null && state.popularMediaSectionUiState.mediaItems.isNotEmpty()
+                isVisible = state.error == null
             )
 
             continueWatchingSection(
@@ -205,6 +164,14 @@ private fun HomeScreenContent(
                 }
             }
         }
+
+        HomeAppBar(
+            modifier = Modifier
+                .background(appBarColor)
+                .statusBarsPadding()
+                .padding(horizontal = 16.dp),
+            onSearchClicked = interactionListener::onClickSearch,
+        )
 
         AnimatedSectionVisibility(visible = state.moodPickerUiState.openMovieDialog) {
             MovieMoodPickerDialogDialog(
