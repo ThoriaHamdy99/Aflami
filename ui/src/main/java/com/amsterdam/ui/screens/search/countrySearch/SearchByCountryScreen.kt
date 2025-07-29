2d2ff74476ce92fddcd05984464fbf62afdc5f7b
Eslam <eslam.mohamed.551999@gmail.com>
2025-07-29 13:08:43 +0300

    fix: fix flashing effect and scroll on landscape on SearchByCountryScreen.kt

--- ui/src/main/java/com/amsterdam/ui/screens/search/countrySearch/SearchByCountryScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/search/countrySearch/SearchByCountryScreen.kt
@@ -1,9 +1,5 @@
 package com.amsterdam.ui.screens.search.countrySearch
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,12 +8,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
@@ -113,40 +112,51 @@ private fun SearchByCountryContent(
         }
 
         Box {
-            AnimatedContent(
-                modifier = Modifier.fillMaxSize(),
-                targetState = state,
-                transitionSpec = { fadeIn() togetherWith fadeOut() }) { uiState ->
-                CenterOfScreenContainer(headerHeight) {
-                    when {
-                        uiState.isLoading -> LoadingContainer()
-                        uiState.errorUiState is CountrySearchErrorState.NoNetworkConnection -> {
-                            NoNetworkContainer(
-                                onClickRetry = interactionListener::onClickRetry,
-                                modifier = Modifier.padding(vertical = 8.dp)
-                            )
+            if (
+                state.keyword.isNotBlank() &&
+                !state.isLoading &&
+                state.errorUiState == null &&
+                movies.itemCount != 0 &&
+                !state.showSuggestedCountries
+            ) {
+                MoviesVerticalGrid(
+                    movies = movies,
+                    isVisible = true,
+                    onMovieClicked = interactionListener::onClickMovieCard
+                )
+            } else {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 8.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    CenterOfScreenContainer(unneededSpace = headerHeight) {
+                        when {
+                            state.isLoading -> LoadingContainer()
+                            state.errorUiState is CountrySearchErrorState.NoNetworkConnection -> {
+                                NoNetworkContainer(
+                                    onClickRetry = interactionListener::onClickRetry,
+                                    modifier = Modifier.padding(vertical = 8.dp)
+                                )
+                            }
+
+                            movies.itemCount == 0 &&
+                                    !state.showSuggestedCountries &&
+                                    !state.isLoading &&
+                                    state.selectedCountryIsoCode.isNotBlank() -> NoMoviesFound()
+
+                            movies.itemCount == 0 -> ExploreCountries()
+
+                            else -> {}
                         }
-
-                        movies.itemCount == 0 &&
-                                !uiState.showSuggestedCountries &&
-                                !uiState.isLoading &&
-                                uiState.selectedCountryIsoCode.isNotBlank() -> NoMoviesFound()
-
-                        movies.itemCount == 0 -> ExploreCountries()
-
-                        else ->
-                            MoviesVerticalGrid(
-                                movies = movies,
-                                isVisible = true,
-                                onMovieClicked = interactionListener::onClickMovieCard,
-                            )
-
                     }
                 }
             }
             CountriesDropdownMenu(
                 items = state.suggestedCountries.take(4),
-                isVisible = state.showSuggestedCountries,
+                isVisible = state.showSuggestedCountries && state.suggestedCountries.isNotEmpty(),
                 onItemClicked = interactionListener::onSelectCountry,
                 modifier = Modifier
                     .fillMaxWidth()
