ddecc998732d8a11ef70accd5610167d53f22161
mohamed <hassanmmd4@gmail.com>
2025-07-30 17:22:31 +0300

    refactor:

--- ui/src/main/java/com/amsterdam/ui/screens/movieDetails/MovieDetailsScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/movieDetails/MovieDetailsScreen.kt
@@ -1,12 +1,14 @@
 package com.amsterdam.ui.screens.movieDetails
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,14 +32,32 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceAtLeast
+import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -77,6 +97,7 @@ import com.amsterdam.viewmodel.movieDetails.MovieDetailsUiState.MovieExtras
 import com.amsterdam.viewmodel.movieDetails.MovieDetailsViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @Composable
 fun MovieDetailsScreen(viewModel: MovieDetailsViewModel = hiltViewModel()) {
@@ -118,9 +139,18 @@ fun MovieContent(
 ) {
     val configuration = LocalConfiguration.current
     val screenWidthDp by remember { mutableStateOf(configuration.screenWidthDp.dp) }
+    val screenHeightDp = configuration.screenHeightDp.dp
+    val density = LocalDensity.current
+    val coroutineScope = rememberCoroutineScope()
+
+    var movieExtrasSectionYOffsetDp by remember { mutableStateOf(0.dp) }
+    var movieExtrasSectionHeightDp by remember { mutableStateOf(0.dp) }
+
     val listState = rememberLazyListState()
     val animationDuration by remember { mutableIntStateOf(1000) }
     val pagerState = rememberPagerState { state.moviePostersUrl.size }
+
+
 
     LaunchedEffect(true) {
         while (true) {
@@ -170,7 +200,8 @@ fun MovieContent(
                 Modifier
                     .fillMaxSize()
                     .background(AppTheme.color.surface)
-                    .navigationBarsPadding(),
+                    .navigationBarsPadding()
+            ,
         ) {
             item {
                 Box(
@@ -293,7 +324,13 @@ fun MovieContent(
                                     .background(AppTheme.color.stroke),
                         )
                         MovieExtrasSection(
-                            modifier = Modifier.padding(top = 12.dp),
+                            modifier = Modifier.padding(top = 12.dp)
+                                .onGloballyPositioned { coordinates ->
+                                    with(density) {
+                                        movieExtrasSectionYOffsetDp = coordinates.positionInParent().y.toDp()
+                                        movieExtrasSectionHeightDp = coordinates.size.height.toDp() // Also capture height
+                                    }
+                                },
                             extras = state.extraItem,
                             onClickExtras = interactionListener::onClickMovieExtras,
                         )
@@ -312,6 +349,32 @@ fun MovieContent(
                         MovieExtras.COMPANY_PRODUCTION -> CompanyProductionSection(state.productionCompany)
                     }
                 }
+
+            item {
+                val lastVisibleItemInfo by remember { derivedStateOf { listState.layoutInfo.visibleItemsInfo.lastOrNull() } }
+                val totalItemsCount by remember { derivedStateOf { listState.layoutInfo.totalItemsCount } }
+
+                val viewportHeightDp by remember {
+                    derivedStateOf {
+                        with(density) { listState.layoutInfo.visibleItemsInfo.last().offset.toDp() }
+                    }
+                }
+
+                val isSpacerLastLayoutItem = totalItemsCount > 0 && lastVisibleItemInfo?.index == totalItemsCount - 1
+
+
+                val spacerHeight: Dp = if (isSpacerLastLayoutItem) {
+                    Log.d("column", "screen height: $screenHeightDp")
+                    Log.d("column", "offset: $viewportHeightDp")
+                    val remainingSpaceInViewport = screenHeightDp - viewportHeightDp
+                    Log.d("column", "remaining: $remainingSpaceInViewport")
+                    remainingSpaceInViewport.coerceAtLeast(0.dp).coerceAtMost(screenHeightDp)
+                } else {
+                    0.dp
+                }
+
+                Spacer(modifier = Modifier.height(spacerHeight))
+            }
         }
     }
 }
