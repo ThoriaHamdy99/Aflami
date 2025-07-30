8cc8054638889250feec950c0ea492f104c6a005
mohamed <hassanmmd4@gmail.com>
2025-07-30 23:38:09 +0300

    feature: add throttled navigation effect in base viewmodel and map the effect to not null

--- viewModel/src/main/java/com/amsterdam/viewmodel/shared/BaseViewModel.kt
+++ viewModel/src/main/java/com/amsterdam/viewmodel/shared/BaseViewModel.kt
@@ -6,13 +6,17 @@ import com.amsterdam.domain.exceptions.AflamiException
 import com.amsterdam.viewmodel.utils.dispatcher.DispatcherProvider
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
 
 open class BaseViewModel<S, E>(
     initialState: S,
@@ -24,8 +28,10 @@ open class BaseViewModel<S, E>(
     val state: StateFlow<S> = _state.asStateFlow()
 
     private val _effect = MutableSharedFlow<E?>()
-    val effect = _effect.asSharedFlow()
+    val effect = _effect.asSharedFlow().mapNotNull { it }
 
+    private val _navigationEffect = MutableSharedFlow<E>()
+    val navigationEffect = _navigationEffect.asSharedFlow().throttleFirst(500).mapNotNull { it }
 
     protected fun updateState(updater: (S) -> S) {
         viewModelScope.launch(dispatcherProvider.MainImmediate) {
@@ -36,6 +42,12 @@ open class BaseViewModel<S, E>(
     protected fun sendNewEffect(newEffect: E) {
         viewModelScope.launch(dispatcherProvider.Default) {
             _effect.emit(newEffect)
+        }
+    }
+
+    protected fun sendNewNavigationEffect(navigationEffect: E) {
+        viewModelScope.launch(dispatcherProvider.Default) {
+            _navigationEffect.emit(navigationEffect)
         }
     }
 
@@ -57,6 +69,20 @@ open class BaseViewModel<S, E>(
                 onError(AflamiException())
             } finally {
                 onCompletion()
+            }
+        }
+    }
+
+    private fun <T> Flow<T>.throttleFirst(periodMillis: Long): Flow<T> {
+        require(periodMillis > 0)
+        return flow {
+            var lastTime = 0L
+            collect { value ->
+                val currentTime = Clock.System.now().toEpochMilliseconds()
+                if (currentTime - lastTime >= periodMillis) {
+                    lastTime = currentTime
+                    emit(value)
+                }
             }
         }
     }
