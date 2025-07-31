ca8cef689bb8f62d968cf065c7f756d527c83079
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-31 12:44:13 +0300

    fis: series details screen UI and hide empty fields

--- ui/src/main/java/com/amsterdam/ui/screens/seriesDetails/SeriesDetailsScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/seriesDetails/SeriesDetailsScreen.kt
@@ -1,11 +1,14 @@
 package com.amsterdam.ui.screens.seriesDetails
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -41,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionOnScreen
 import androidx.compose.ui.platform.LocalConfiguration
@@ -289,17 +293,21 @@ fun SeriesDetailsContent(
                                 color = AppTheme.color.title,
                                 modifier = Modifier.padding(horizontal = 16.dp)
                             )
-                            LazyRow(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 12.dp),
-                                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                contentPadding = PaddingValues(horizontal = 16.dp)
-                            ) {
-                                items(state.categories) {
-                                    CategoryChip(categoryName = getTvShowGenreLabel(it))
+
+                            if(state.categories.isNotEmpty()){
+                                LazyRow(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 12.dp),
+                                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                                    contentPadding = PaddingValues(horizontal = 16.dp)
+                                ) {
+                                    items(state.categories) {
+                                        CategoryChip(categoryName = getTvShowGenreLabel(it))
+                                    }
                                 }
                             }
+
                             SeriesInfoSection(
                                 modifier = Modifier
                                     .padding(top = 8.dp)
@@ -308,12 +316,14 @@ fun SeriesDetailsContent(
                                 seasonCount = state.seasonCount,
                                 originCountry = state.originCountry,
                             )
+
                             DescriptionSection(
                                 modifier = Modifier
                                     .padding(top = 24.dp)
-                                    .padding(horizontal = 24.dp),
+                                    .padding(horizontal = 16.dp),
                                 description = state.description
                             )
+
                             CastSection(
                                 modifier = Modifier.padding(top = 24.dp),
                                 actors = state.cast,
@@ -400,28 +410,36 @@ private fun SeriesInfoSection(
 ) {
     val items = listOf(airDate, seasonCount, originCountry)
 
-    Row(
-        modifier = modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items.forEachIndexed { index, item ->
-            Text(
-                text = item,
-                style = AppTheme.textStyle.label.small,
-                color = AppTheme.color.hint
-            )
+    if (!items.all{it.isEmpty()}) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items.forEachIndexed { index, item ->
+                AnimatedVisibility(
+                    visible = item.isNotEmpty(),
+                    enter = slideInVertically(),
+                    exit = slideOutVertically()
+                ) {
+                    Text(
+                        text = item,
+                        style = AppTheme.textStyle.label.small,
+                        color = AppTheme.color.hint
+                    )
 
-            if (index < items.lastIndex) {
-                Box(
-                    modifier = Modifier
-                        .size(4.dp)
-                        .background(AppTheme.color.stroke, shape = CircleShape)
-                )
+                    if (index < items.lastIndex) {
+                        Box(
+                            modifier = Modifier
+                                .size(4.dp)
+                                .background(AppTheme.color.stroke, shape = CircleShape)
+                        )
+                    }
+                }
             }
         }
     }
+
 }
 
 @Composable
