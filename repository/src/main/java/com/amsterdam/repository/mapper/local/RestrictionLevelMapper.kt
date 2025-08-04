3fec89e057b48be9874133db7e48751ad27d7b52
mohamed <hassanmmd4@gmail.com>
2025-08-04 22:46:16 +0300

    refactor: use functions for mapping for consistency

--- repository/src/main/java/com/amsterdam/repository/mapper/local/RestrictionLevelMapper.kt
+++ repository/src/main/java/com/amsterdam/repository/mapper/local/RestrictionLevelMapper.kt
@@ -3,35 +3,26 @@ package com.amsterdam.repository.mapper.local
 import com.amsterdam.domain.utils.RestrictionLevel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
 
-class RestrictionLevelMapper @Inject constructor() {
-    fun toLocalRestrictionLevel(restrictionLevel: RestrictionLevel): String {
-        return when (restrictionLevel) {
-            RestrictionLevel.STRICT -> STRICT
-            RestrictionLevel.MODERATE -> MODERATE
-            RestrictionLevel.OFF -> OFF
-        }
+
+fun RestrictionLevel.toLocalDto(): String {
+    return when (this) {
+        RestrictionLevel.STRICT -> "STRICT"
+        RestrictionLevel.MODERATE -> "MODERATE"
+        RestrictionLevel.OFF -> "OFF"
     }
+}
 
-    fun fromLocalRestrictionLevel(restrictionLevel: Flow<String>): Flow<RestrictionLevel> {
-        return flow {
-            restrictionLevel.collect {
-                emit(
-                    when(it){
-                        STRICT -> RestrictionLevel.STRICT
-                        MODERATE -> RestrictionLevel.MODERATE
-                        else -> RestrictionLevel.OFF
-                    }
-                )
-            }
+fun stringToRestrictionLevelEntity(restrictionLevel: Flow<String>): Flow<RestrictionLevel> {
+    return flow {
+        restrictionLevel.collect {
+            emit(
+                when (it) {
+                    "STRICT" -> RestrictionLevel.STRICT
+                    "MODERATE" -> RestrictionLevel.MODERATE
+                    else -> RestrictionLevel.OFF
+                }
+            )
         }
-
-    }
-
-    private companion object {
-        private const val STRICT = "STRICT"
-        private const val MODERATE = "MODERATE"
-        private const val OFF = "OFF"
     }
 }
--- repository/src/main/java/com/amsterdam/repository/repository/AppPreferencesRepositoryImpl.kt
+++ repository/src/main/java/com/amsterdam/repository/repository/AppPreferencesRepositoryImpl.kt
@@ -3,13 +3,13 @@ package com.amsterdam.repository.repository
 import com.amsterdam.domain.repository.AppPreferencesRepository
 import com.amsterdam.domain.utils.RestrictionLevel
 import com.amsterdam.repository.datasource.local.AppPreferences
-import com.amsterdam.repository.mapper.local.RestrictionLevelMapper
+import com.amsterdam.repository.mapper.local.stringToRestrictionLevelEntity
+import com.amsterdam.repository.mapper.local.toLocalDto
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class AppPreferencesRepositoryImpl @Inject constructor(
     private val preferences: AppPreferences,
-    private val restrictionLevelMapper: RestrictionLevelMapper
 ) : AppPreferencesRepository {
     override fun getDeviceLanguage(): Flow<String> = preferences.getDeviceLanguage()
 
@@ -25,10 +25,10 @@ class AppPreferencesRepositoryImpl @Inject constructor(
     }
 
     override fun getRestrictionLevel(): Flow<RestrictionLevel> {
-        return restrictionLevelMapper.fromLocalRestrictionLevel(preferences.getRestrictionLevel())
+        return stringToRestrictionLevelEntity(preferences.getRestrictionLevel())
     }
 
     override suspend fun setRestrictionLevel(restrictionLevel: RestrictionLevel) {
-        preferences.setRestrictionLevel(restrictionLevelMapper.toLocalRestrictionLevel(restrictionLevel))
+        preferences.setRestrictionLevel(restrictionLevel.toLocalDto())
     }
 }
