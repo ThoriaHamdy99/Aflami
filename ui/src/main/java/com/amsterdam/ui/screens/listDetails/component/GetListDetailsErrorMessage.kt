5cb5d898f0267958f0785516250ccf15334cafcc
Ahmed Mahgoub <aed7862@gmail.com>
2025-08-05 01:02:38 +0300

    refactor: delete log statement

--- ui/src/main/java/com/amsterdam/ui/screens/listDetails/component/GetListDetailsErrorMessage.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/listDetails/component/GetListDetailsErrorMessage.kt
@@ -6,7 +6,6 @@ import com.amsterdam.ui.R
 import com.amsterdam.viewmodel.listDetails.ListDetailsError
 
 fun getListDetailsErrorMessage(listDetailsError: ListDetailsError?, context: Context): String{
-        Log.e("ListDetailsViewModel", "getListDetailsErrorMessage: $listDetailsError")
     return when(listDetailsError){
         ListDetailsError.NoNetwork -> context.getString(R.string.offline_message)
         else -> ""
