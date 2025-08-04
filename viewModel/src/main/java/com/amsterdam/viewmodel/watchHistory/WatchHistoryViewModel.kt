178987339ac8eae0242e7881e07cc518d503b640
saraashraf11 <saraashraf1822004@gmail.com>
2025-08-04 03:07:43 +0300

    feature: add WatchHistoryViewModel

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/watchHistory/WatchHistoryViewModel.kt
@@ -0,0 +1,156 @@
+package com.amsterdam.viewmodel.watchHistory
+
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.amsterdam.domain.exceptions.AflamiException
+import com.amsterdam.domain.exceptions.NoInternetException
+import com.amsterdam.domain.useCase.home.GetContinueWatchingMoviesUseCase
+import com.amsterdam.domain.useCase.home.GetContinueWatchingTvShowsUseCase
+import com.amsterdam.domain.useCase.preferences.ManageLocaleLanguageUseCase
+import com.amsterdam.paging.PagingSource
+import com.amsterdam.viewmodel.search.keywordSearch.TabOption
+import com.amsterdam.viewmodel.search.mapper.toMediaItemUiState
+import com.amsterdam.viewmodel.shared.BaseViewModel
+import com.amsterdam.viewmodel.shared.uiStates.MovieItemUiState
+import com.amsterdam.viewmodel.shared.uiStates.TvShowItemUiState
+import com.amsterdam.viewmodel.shared.uiStates.media.MediaType
+import com.amsterdam.viewmodel.utils.dispatcher.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class WatchHistoryViewModel @Inject constructor(
+    private val getContinueWatchingMoviesUseCase: GetContinueWatchingMoviesUseCase,
+    private val getContinueWatchingTvShowsUseCase: GetContinueWatchingTvShowsUseCase,
+    manageLocaleLanguageUseCase: ManageLocaleLanguageUseCase,
+    dispatcherProvider: DispatcherProvider
+) : BaseViewModel<WatchHistoryUiState, WatchHistoryEffect>(
+    WatchHistoryUiState(),
+    dispatcherProvider
+),
+    WatchHistoryInteractionListener {
+
+    init {
+        manageLocaleLanguageUseCase.getDeviceLanguage()
+            .onEach {
+                getMoviesWatchHistoryData()
+            }.launchIn(viewModelScope)
+        getMoviesWatchHistoryData()
+    }
+
+    private fun getMoviesWatchHistoryData() {
+        updateState { it.copy(isLoading = true) }
+        tryToExecute(
+            action = {
+                Pager(
+                    config = PagingConfig(pageSize = 20),
+                    pagingSourceFactory = {
+                        PagingSource { page ->
+                            getContinueWatchingMoviesUseCase(page = page, pageSize = 20).first()
+                        }
+                    }
+                ).flow.map { it.map { it.movie.toMediaItemUiState() } }.cachedIn(viewModelScope)
+            },
+            onSuccess = ::onGetMoviesContinueWatchingDataSuccess,
+            onError = ::onError,
+            onCompletion = ::onCompletion
+        )
+    }
+
+    private fun getTvShowsWatchHistoryData() {
+        updateState { it.copy(isLoading = true) }
+        tryToExecute(
+            action = {
+                Pager(
+                    config = PagingConfig(pageSize = 20),
+                    pagingSourceFactory = {
+                        PagingSource { page ->
+                            getContinueWatchingTvShowsUseCase(page = page, pageSize = 20).first()
+                        }
+                    }
+                ).flow.map { it.map { it.tvShow.toMediaItemUiState() } }.cachedIn(viewModelScope)
+            },
+            onSuccess = ::onGetTvShowContinueWatchingDataSuccess,
+            onError = ::onError,
+            onCompletion = ::onCompletion
+        )
+    }
+
+    fun onGetMoviesContinueWatchingDataSuccess(
+        mediaItems:
+        Flow<PagingData<MovieItemUiState>>
+    ) {
+        updateState { currentState ->
+            currentState.copy(
+                movies = mediaItems
+            )
+        }
+    }
+
+    fun onGetTvShowContinueWatchingDataSuccess(
+        mediaItems:
+        Flow<PagingData<TvShowItemUiState>>
+    ) {
+        updateState { currentState ->
+            currentState.copy(
+                tvShows = mediaItems
+            )
+        }
+    }
+
+    private fun onError(exception: AflamiException) {
+        when (exception) {
+            is NoInternetException -> updateState {
+                it.copy(
+                    error = WatchHistoryUiState.WatchHistoryError.NetworkError
+                )
+            }
+
+            else -> {}
+        }
+    }
+
+    override fun onClickMediaItem(mediaId: Long, mediaType: MediaType) {
+        if (mediaType == MediaType.MOVIE)
+            sendNewEffect(WatchHistoryEffect.NavigateToMovieDetails(mediaId))
+        else
+            sendNewEffect(WatchHistoryEffect.NavigateToTvShowDetails(mediaId))
+    }
+
+    override fun onClickTabOption(option: TabOption) {
+        _state.update { it.copy(selectedTabOption = option) }
+        if (option == TabOption.MOVIES) {
+            getMoviesWatchHistoryData()
+        } else {
+            getTvShowsWatchHistoryData()
+        }
+    }
+
+    override fun onClickRetryLoading() {
+        getMoviesWatchHistoryData()
+    }
+
+    override fun onClickMovieCard(movieId: Long) {
+        sendNewNavigationEffect(WatchHistoryEffect.NavigateToMovieDetails(movieId))
+    }
+
+    override fun onClickTvShowCard(tvShowId: Long) {
+        sendNewNavigationEffect(WatchHistoryEffect.NavigateToTvShowDetails(tvShowId))
+    }
+
+    override fun onClickBack() {
+        sendNewEffect(WatchHistoryEffect.NavigateBack)
+    }
+
+    private fun onCompletion() = updateState { it.copy(isLoading = false) }
+}
