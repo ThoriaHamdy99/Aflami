8481a10f93c35078636ea34a0ff8fbd0bedb51f4
Thoraya <thoriahamdy99@gmail.com>
2025-08-04 20:39:10 +0300

    feature: add selection items components in screen components to be used.

---
+++ ui/src/main/java/com/amsterdam/ui/components/selection/AnswerSelectionItem.kt
@@ -0,0 +1,90 @@
+package com.amsterdam.ui.components.selection
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.amsterdam.designsystem.R
+import com.amsterdam.designsystem.components.Icon
+import com.amsterdam.designsystem.components.Text
+import com.amsterdam.designsystem.theme.AflamiTheme
+import com.amsterdam.designsystem.theme.AppTheme
+import com.amsterdam.designsystem.utils.ThemeAndLocalePreviews
+
+@Composable
+fun AnswerSelectionItem(
+    modifier: Modifier = Modifier,
+    text: String = "",
+    status: AnswerStatus = AnswerStatus.Unselected,
+    onClick: () -> Unit = {}
+) {
+    val shape = RoundedCornerShape(16.dp)
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = status.backgroundColor(),
+                shape = shape,
+            )
+            .border(
+                color = status.backgroundColor(),
+                width = 1.dp,
+                shape = shape,
+            )
+            .clip(shape)
+            .clickable(onClick = onClick)
+            .padding(vertical = 16.dp, horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Text(
+            text = text,
+            color = AppTheme.color.body,
+            style = AppTheme.textStyle.label.large,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+        )
+        Icon(
+            painter = painterResource(status.icon),
+            contentDescription = null,
+            tint = status.borderColor()
+        )
+    }
+}
+
+@Composable
+@ThemeAndLocalePreviews
+private fun LanguageSelectionItemPreview() {
+    AflamiTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            AnswerSelectionItem(
+                text = stringResource(R.string.light),
+                status = AnswerStatus.Unselected,
+            )
+            AnswerSelectionItem(
+                text = stringResource(R.string.light),
+                status = AnswerStatus.Correct,
+            )
+            AnswerSelectionItem(
+                text = stringResource(R.string.light),
+                status = AnswerStatus.Wrong,
+            )
+        }
+    }
+}
