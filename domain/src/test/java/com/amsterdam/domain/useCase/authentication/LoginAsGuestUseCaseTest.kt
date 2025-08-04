0fba75bd321406c795c59bbfcaf21fbde2a29eb5
Nabil Bouzine <139881384+nabilBouzineDev@users.noreply.github.com>
2025-08-04 23:28:22 +0100

    Increase domain module test coverage and add new test cases (#340)
    * test: add unit tests for GetsSessionType and LoginAsGuestUseCase
    
    * refactor: fix broken and failed unit tests in RecentSearchesUseCaseTest
    
    * test: add unit tests for AppPreferencesRepository use cases

---
+++ domain/src/test/java/com/amsterdam/domain/useCase/authentication/LoginAsGuestUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.amsterdam.domain.useCase.authentication
+
+import com.amsterdam.domain.repository.AuthenticationRepository
+import com.amsterdam.domain.utils.SessionType
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LoginAsGuestUseCaseTest {
+
+    private lateinit var loginAsGuestUseCase: LoginAsGuestUseCase
+    private val authenticationRepository: AuthenticationRepository = mockk(relaxed = true)
+
+    @BeforeEach
+    fun setUp() {
+        loginAsGuestUseCase = LoginAsGuestUseCase(authenticationRepository)
+    }
+
+    @Test
+    fun `should call setSessionType when login as a GUEST`() = runTest {
+        loginAsGuestUseCase()
+        coVerify(exactly = 1) { authenticationRepository.setSessionType(SessionType.GUEST) }
+    }
+}
