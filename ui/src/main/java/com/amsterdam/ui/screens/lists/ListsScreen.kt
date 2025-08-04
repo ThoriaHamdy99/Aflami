cdcda108aee173b8e9adb7d64ec996e43d20a4be
marah222 <maraharam222@gmail.com>
2025-08-04 18:49:25 +0300

    fix: lists screen UI

--- ui/src/main/java/com/amsterdam/ui/screens/lists/ListsScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/lists/ListsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layosut.size
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
