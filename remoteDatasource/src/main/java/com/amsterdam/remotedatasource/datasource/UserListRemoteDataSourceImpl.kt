9b6a0c4d364aab9c94c9fda2f68aa77b61580754
Abdallah Bakhet <abakhet2010@gmail.com>
2025-08-05 10:26:08 +0300

    refactor: rename UserListRemoteSourceImpl to UserListRemoteDataSourceImpl

--- remoteDatasource/src/main/java/com/amsterdam/remotedatasource/datasource/UserListRemoteDataSourceImpl.kt
+++ remoteDatasource/src/main/java/com/amsterdam/remotedatasource/datasource/UserListRemoteDataSourceImpl.kt
@@ -1,16 +1,15 @@
 package com.amsterdam.remotedatasource.datasource
 
-import android.util.Log
 import com.amsterdam.remotedatasource.api.UserListApiService
 import com.amsterdam.remotedatasource.utils.apiHandler.responseCall
 import com.amsterdam.repository.datasource.remote.UserListRemoteSource
 import com.amsterdam.repository.dto.remote.RemoteUserListResponse
+import com.amsterdam.repository.dto.remote.UserListDetailsResponse
 import javax.inject.Inject
 
 class UserListRemoteDataSourceImpl @Inject constructor(
     private val userListApiService: UserListApiService
 ) : UserListRemoteSource {
-    
     override suspend fun getUserLists(
         accountId: Int,
         page: Int,
@@ -18,4 +17,24 @@ class UserListRemoteDataSourceImpl @Inject constructor(
     ): RemoteUserListResponse {
         return responseCall {  userListApiService.getUserLists(accountId, page, sessionId) }
     }
-} 
+
+    override suspend fun getMoviesFromList(
+        listId: Long,
+        page: Int,
+    ): UserListDetailsResponse = responseCall { userListApiService.getMoviesFromList(listId, page) }
+
+    override suspend fun deleteList(
+        listId: Long,
+        sessionId: String,
+    ) {
+        responseCall { userListApiService.deleteList(listId, sessionId) }
+    }
+
+    override suspend fun removeMovieFromList(
+        listId: Long,
+        sessionId: String,
+        movieId: Long,
+    ) {
+        responseCall { userListApiService.removeMovieFromList(listId, sessionId, movieId) }
+    }
+}
