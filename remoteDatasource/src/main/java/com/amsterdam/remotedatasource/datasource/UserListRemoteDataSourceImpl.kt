0296f03074b2dc5b0999561c58fd861e47281665
Ahmed Mahgoub <aed7862@gmail.com>
2025-08-07 14:22:56 +0300

    fix: use response call function with list

--- remoteDatasource/src/main/java/com/amsterdam/remotedatasource/datasource/UserListRemoteDataSourceImpl.kt
+++ remoteDatasource/src/main/java/com/amsterdam/remotedatasource/datasource/UserListRemoteDataSourceImpl.kt
@@ -7,58 +7,67 @@ import com.amsterdam.repository.dto.remote.AddItemToListResponse
 import com.amsterdam.repository.dto.remote.CreateUserListResponse
 import com.amsterdam.repository.dto.remote.RemoteUserListResponse
 import com.amsterdam.repository.dto.remote.UserListDetailsResponse
+import com.amsterdam.repository.dto.remote.authentication.AuthenticationResponseDto
+import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 class UserListRemoteDataSourceImpl @Inject constructor(
-    private val userListApiService: UserListApiService
+    private val userListApiService: UserListApiService,
+    private val json: Json
 ) : UserListRemoteSource {
     override suspend fun createNewList(
         listName: String,
         description: String,
         language: String,
         sessionId: String,
-    ): CreateUserListResponse =
-        responseCall {
-            userListApiService
-                .createNewList(
-                    sessionId = sessionId,
-                    listName = listName,
-                    description = description,
-                    language = language,
-                )
+    ): CreateUserListResponse {
+        return responseCall {
+            userListApiService.createNewList(
+                sessionId = sessionId,
+                listName = listName,
+                description = description,
+                language = language,
+            )
         }
+    }
 
     override suspend fun getUserLists(
         accountId: Int,
         page: Int,
         sessionId: String
     ): RemoteUserListResponse {
-        return responseCall {  userListApiService.getUserLists(accountId, page, sessionId) }
+        return responseCall({ userListApiService.getUserLists(accountId, page, sessionId) }) {
+            val response = json.decodeFromString<AuthenticationResponseDto>(it)
+            response.statusCode!!
+        }
     }
 
     override suspend fun addMovieToList(
         listId: Long,
         sessionId: String,
         movieId: Int,
-    ): AddItemToListResponse = responseCall { userListApiService.addMediaItemToList(listId, sessionId, movieId) }
-
-    override suspend fun getMoviesFromList(
-        listId: Long,
-        page: Int,
-    ): UserListDetailsResponse = responseCall { userListApiService.getMoviesFromList(listId, page) }
-
-    override suspend fun deleteList(
-        listId: Long,
-        sessionId: String,
-    ) {
-        responseCall { userListApiService.deleteList(listId, sessionId) }
+    ): AddItemToListResponse {
+        return responseCall { userListApiService.addMediaItemToList(listId, sessionId, movieId) }
     }
 
-    override suspend fun removeMovieFromList(
-        listId: Long,
-        sessionId: String,
-        movieId: Long,
-    ) {
-        responseCall { userListApiService.removeMovieFromList(listId, sessionId, movieId) }
+    override suspend fun getMoviesFromList(listId: Long, page: Int): UserListDetailsResponse {
+        return responseCall({ userListApiService.getMoviesFromList(listId, page) }) {
+            val response = json.decodeFromString<AuthenticationResponseDto>(it)
+            response.statusCode!!
+        }
+    }
+
+    override suspend fun deleteList(listId: Long, sessionId: String) {
+        responseCall({ userListApiService.deleteList(listId, sessionId) }) {
+            val response = json.decodeFromString<AuthenticationResponseDto>(it)
+            response.statusCode!!
+        }
+    }
+
+    override suspend fun removeMovieFromList(listId: Long, sessionId: String, movieId: Long) {
+        responseCall({ userListApiService.removeMovieFromList(listId, sessionId, movieId) }) {
+            val response = json.decodeFromString<AuthenticationResponseDto>(it)
+            response.statusCode!!
+        }
     }
 }
