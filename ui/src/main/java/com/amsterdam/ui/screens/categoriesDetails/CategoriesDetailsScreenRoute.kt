5dea4a82c44dffa249739ca37aad8228f4c7a93a
Mona <monaayman584@gmail.com>
2025-08-10 23:08:57 +0300

    feature: add CategoriesDetailsScreen.kt

---
+++ ui/src/main/java/com/amsterdam/ui/screens/categoriesDetails/CategoriesDetailsScreenRoute.kt
@@ -0,0 +1,12 @@
+package com.amsterdam.ui.screens.categoriesDetails
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.amsterdam.ui.navigation.Route
+
+fun NavGraphBuilder.categoriesDetailsScreenRoute(){
+    composable<Route.CategoriesDetails> {
+        CategoriesDetailsScreen()
+    }
+
+}
