f4e216891d161776aca8cfd7aa0902dd1066f816
Mona <monaayman584@gmail.com>
2025-08-10 23:07:55 +0300

    feature: create CategoriesViewModel.kt

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/categories/CategoriesViewModel.kt
@@ -0,0 +1,31 @@
+package com.amsterdam.viewmodel.categories
+
+import com.amsterdam.viewmodel.shared.BaseViewModel
+import com.amsterdam.viewmodel.shared.TabOption
+import com.amsterdam.viewmodel.utils.dispatcher.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+
+@HiltViewModel
+class CategoriesViewModel @Inject constructor(
+    dispatcherProvider: DispatcherProvider
+) : BaseViewModel<CategoriesUiState, CategoriesUiEffect>
+    (CategoriesUiState(), dispatcherProvider), CategoriesInteractionListener {
+
+
+    override fun onChangeTabOption(tabOption: TabOption) {
+        updateState {
+            it.copy(selectedTabOption = tabOption)
+        }
+
+    }
+
+    override fun onNavigateCategoriesDetailsScreen(
+        genreName: String,
+        mediaType: String
+    ) {
+        sendNewNavigationEffect(CategoriesUiEffect.NavigateCategoriesDetailsScreen(genreName,mediaType))
+    }
+
+    }
+
