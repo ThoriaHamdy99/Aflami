34eda8f9b49548a41f184e68cf63094ac1380252
Mona <monaayman584@gmail.com>
2025-08-10 23:09:57 +0300

    feature: add  CategoriesDetailsUiState.kt

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/categoriesDetails/CategoriesDetailsUiState.kt
@@ -0,0 +1,40 @@
+package com.amsterdam.viewmodel.categoriesDetails
+
+import com.amsterdam.domain.exceptions.NetworkException
+import com.amsterdam.viewmodel.shared.defaultMovieGenres
+import com.amsterdam.viewmodel.shared.uiStates.MediaType
+import com.amsterdam.viewmodel.shared.uiStates.MovieGenreItemUiState
+import com.amsterdam.viewmodel.shared.uiStates.TvGenreItemUiState
+
+
+data class CategoriesDetailsUiState(
+    val mediaType: MediaType = MediaType.MOVIE,
+    val selectedGenreName: String = "",
+    val appBarTitle: String = "",
+    val movieGenres: List<MovieGenreItemUiState> = defaultMovieGenres,
+    val tvShowGenres: List<TvGenreItemUiState> = emptyList(),
+    val movies: List<MoviesUiState> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorUiState: CategoriesDetailsErrorState? = null,
+) {
+    data class MoviesUiState(
+        val id: Long = 0,
+        val name: String = "",
+        val posterImageUrl: String = "",
+        val rate: String = "",
+        val yearOfRelease: String = "",
+        val mediaType: MediaType = MediaType.MOVIE
+    )
+    sealed interface CategoriesDetailsErrorState {
+        data object NoNetworkConnection : CategoriesDetailsErrorState
+        data object UnknownError : CategoriesDetailsErrorState
+        companion object {
+            fun toSearchErrorState(exception: Throwable): CategoriesDetailsErrorState =
+                when (exception) {
+                    is NetworkException -> NoNetworkConnection
+                    else -> UnknownError
+                }
+        }
+
+    }
+}
