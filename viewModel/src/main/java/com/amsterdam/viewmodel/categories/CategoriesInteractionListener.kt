85ce0c71db38582f5a2b2decf65b2405bb02bb15
Mona <monaayman584@gmail.com>
2025-08-10 23:10:28 +0300

    feature: add  CategoriesInteractionListener.kt

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/categories/CategoriesInteractionListener.kt
@@ -0,0 +1,10 @@
+package com.amsterdam.viewmodel.categories
+
+import com.amsterdam.viewmodel.shared.TabOption
+
+interface CategoriesInteractionListener {
+    fun onChangeTabOption(tabOption: TabOption)
+    fun onNavigateCategoriesDetailsScreen(genreName: String,mediaType: String)
+
+
+}
