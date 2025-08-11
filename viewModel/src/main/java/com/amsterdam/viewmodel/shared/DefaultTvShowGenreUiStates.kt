897bc773c60f98d0319c8534054374a3098cf62a
Mona <monaayman584@gmail.com>
2025-08-11 17:57:43 +0300

    feature: add DefaultTvShowGenreUiStates.kt

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/shared/DefaultTvShowGenreUiStates.kt
@@ -0,0 +1,15 @@
+package com.amsterdam.viewmodel.shared
+
+import com.amsterdam.entity.category.TvShowGenre
+import com.amsterdam.viewmodel.shared.uiStates.TvShowGenreItemUiState
+
+val defaultTvShowGenres =
+    TvShowGenre.entries.toTypedArray().mapIndexed { index, category ->
+        TvShowGenreItemUiState(
+            selectableTvShowGenre =
+                Selectable(
+                    item = category,
+                    isSelected = index == 0,
+                )
+        )
+    }
