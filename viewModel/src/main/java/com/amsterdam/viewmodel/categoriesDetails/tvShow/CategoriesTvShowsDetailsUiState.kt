f41b5977032e3e01381f26ab0ee5a71b8b471080
Mona <monaayman584@gmail.com>
2025-08-11 17:57:09 +0300

    feature: add CategoriesTvShowsDetailsUiState.kt

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/categoriesDetails/tvShow/CategoriesTvShowsDetailsUiState.kt
@@ -0,0 +1,37 @@
+package com.amsterdam.viewmodel.categoriesDetails.tvShow
+
+import androidx.paging.PagingData
+import com.amsterdam.domain.exceptions.NetworkException
+import com.amsterdam.viewmodel.shared.defaultTvShowGenres
+import com.amsterdam.viewmodel.shared.uiStates.TvShowGenreItemUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class CategoriesTvShowsDetailsUiState(
+    val selectedGenreName: String = "",
+    val tvShowGenres: List<TvShowGenreItemUiState> = defaultTvShowGenres,
+    val tvShows: Flow<PagingData<TvShowsUiState>> = emptyFlow(),
+    val isLoading: Boolean = false,
+    val errorUiState: CategoriesTvShowsDetailsErrorState? = null,
+) {
+    sealed interface CategoriesTvShowsDetailsErrorState {
+        data object NoNetworkConnection : CategoriesTvShowsDetailsErrorState
+        data object UnknownError : CategoriesTvShowsDetailsErrorState
+        companion object {
+            fun toSearchErrorState(exception: Throwable): CategoriesTvShowsDetailsErrorState =
+                when (exception) {
+                    is NetworkException -> NoNetworkConnection
+                    else -> UnknownError
+                }
+        }
+    }
+
+
+    data class TvShowsUiState(
+        val id: Long = 0,
+        val name: String = "",
+        val posterImageUrl: String = "",
+        val rate: String = "",
+        val yearOfRelease: String = "",
+    )
+}
