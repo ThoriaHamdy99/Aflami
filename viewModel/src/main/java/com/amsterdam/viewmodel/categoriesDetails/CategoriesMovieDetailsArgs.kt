7dc108bdcfb00ce1a151cfcc0300f158550ae57a
Mona <monaayman584@gmail.com>
2025-08-11 15:00:02 +0300

    refactor: rename file

--- viewModel/src/main/java/com/amsterdam/viewmodel/categoriesDetails/CategoriesMovieDetailsArgs.kt
+++ viewModel/src/main/java/com/amsterdam/viewmodel/categoriesDetails/CategoriesMovieDetailsArgs.kt
@@ -5,14 +5,9 @@ import com.amsterdam.entity.category.MovieGenre
 
 class CategoriesMovieDetailsArgs (savedStateHandle: SavedStateHandle){
     val genre = savedStateHandle.get<String>(GENRE_NAME)
-    val mediaType = savedStateHandle.get<String>(MEDIA_TYPE)
-
 
 
     companion object{
         const val GENRE_NAME = "genreName"
-        const val MEDIA_TYPE = "mediaType"
-
-
     }
 }
