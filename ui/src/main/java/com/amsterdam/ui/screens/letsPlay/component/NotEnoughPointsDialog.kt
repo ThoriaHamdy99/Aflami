c73fd200eb847205d0fb796e6cbdbe1bbdf7f484
saraashraf11 <saraashraf1822004@gmail.com>
2025-08-12 15:03:16 +0300

    refactor: add onConfirm function and delete redundant code

--- ui/src/main/java/com/amsterdam/ui/screens/letsPlay/component/NotEnoughPointsDialog.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/letsPlay/component/NotEnoughPointsDialog.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.amsterdam.designsystem.components.Dialog
 import com.amsterdam.designsystem.components.IconButton
 import com.amsterdam.designsystem.components.Text
@@ -28,67 +27,57 @@ import com.amsterdam.ui.R
 
 @Composable
 fun NotEnoughPointsDialog(
+    modifier: Modifier = Modifier,
+    onConfirm: () -> Unit,
     onDismiss: () -> Unit,
-    modifier: Modifier = Modifier
 ) {
     Dialog(
         onDismiss = onDismiss,
         modifier = modifier
     ) {
-        NotEnoughPointsDialogContent(onDismiss = onDismiss)
-    }
-}
-
-@Composable
-private fun NotEnoughPointsDialogContent(
-    onDismiss: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(16.dp))
-            .padding(horizontal = 12.dp, vertical = 12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(24.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .padding(horizontal = 12.dp, vertical = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.not_enough_points),
+                    style = AppTheme.textStyle.title.medium,
+                    color = AppTheme.color.title,
+                    modifier = Modifier.weight(1f)
+                )
+                IconButton(
+                    painter = painterResource(id = com.amsterdam.designsystem.R.drawable.ic_cancel),
+                    contentDescription = stringResource(id = R.string.cd_cancel),
+                    onClick = onDismiss,
+                    tint = AppTheme.color.title
+                )
+            }
+
             Text(
-                text = stringResource(R.string.not_enough_points),
-                style = AppTheme.textStyle.title.medium,
-                color = AppTheme.color.title,
-                fontSize = 20.sp,
-                modifier = Modifier.weight(1f)
+                text = stringResource(R.string.not_enough_points_description),
+                style = AppTheme.textStyle.body.medium,
+                color = AppTheme.color.body,
             )
-            IconButton(
-                painter = painterResource(id = com.amsterdam.designsystem.R.drawable.ic_cancel),
-                contentDescription = null,
-                onClick = onDismiss,
-                tint = AppTheme.color.title
+
+            ConfirmButton(
+                title = stringResource(R.string.ok),
+                onClick = onConfirm,
+                isEnabled = true,
+                isLoading = false,
+                isNegative = false,
+                modifier = Modifier.fillMaxWidth()
             )
         }
-
-        Text(
-            text = stringResource(R.string.not_enough_points_description),
-            style = AppTheme.textStyle.body.medium,
-            color = AppTheme.color.body,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-        )
-
-        ConfirmButton(
-            title = stringResource(R.string.ok),
-            onClick = onDismiss,
-            isEnabled = true,
-            isLoading = false,
-            isNegative = false,
-            modifier = Modifier.fillMaxWidth()
-        )
     }
 }
 
@@ -110,7 +99,7 @@ private fun NotEnoughPointsDialogPreview() {
                     .background(AppTheme.color.surface)
                     .padding(16.dp)
             ) {
-                NotEnoughPointsDialogContent(onDismiss = {})
+                NotEnoughPointsDialog(onConfirm = {}, onDismiss = {})
             }
         }
     }
