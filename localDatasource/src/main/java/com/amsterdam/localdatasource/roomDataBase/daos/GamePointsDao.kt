06940be162c43fac46d31cc2568fdf4651dc38f9
eslam-99 <eslam.mohamed.551999@gmail.com>
2025-08-12 16:12:14 +0300

    Feature local game points (#457)
    * feature: add game points local datasource
    
    * feature: add game points repository
    
    * test: add game points dao test
    
    * refactor: rename invalid daodao to dao
    
    * refactor: remove suspend from flow functions

---
+++ localDatasource/src/main/java/com/amsterdam/localdatasource/roomDataBase/daos/GamePointsDao.kt
@@ -0,0 +1,17 @@
+package com.amsterdam.localdatasource.roomDataBase.daos
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.amsterdam.repository.dto.local.GamePointsDto
+import com.amsterdam.repository.dto.local.utils.DatabaseConstants
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GamePointsDao {
+    @Upsert
+    suspend fun upsertPoints(gamePoints: GamePointsDto)
+
+    @Query("SELECT points FROM ${DatabaseConstants.GAME_POINTS_TABLE} LIMIT 1")
+    fun getPoints(): Flow<Int?>
+}
