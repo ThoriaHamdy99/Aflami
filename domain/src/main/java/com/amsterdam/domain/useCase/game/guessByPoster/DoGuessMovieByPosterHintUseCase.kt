e18495a86e5e8e5ea2fb9a1a5d39c5f448667315
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-13 16:26:12 +0300

    feat: implement hint functionality for Guess Movie by Poster game

---
+++ domain/src/main/java/com/amsterdam/domain/useCase/game/guessByPoster/DoGuessMovieByPosterHintUseCase.kt
@@ -0,0 +1,38 @@
+package com.amsterdam.domain.useCase.game.guessByPoster
+
+import com.amsterdam.domain.exceptions.NotEnoughPointsException
+import com.amsterdam.domain.useCase.game.GetTotalUserPointsUseCase
+import com.amsterdam.domain.useCase.game.UpdateUserGamePointsUseCase
+
+
+class DoGuessMovieByPosterHintUseCase(
+    private val getTotalUserPointsUseCase: GetTotalUserPointsUseCase,
+    private val updatePoints: UpdateUserGamePointsUseCase
+) {
+    suspend operator fun invoke(
+        moviePosterQuestion: MoviePosterQuestion
+    ): MoviePosterQuestion {
+
+        val currentPoints = getTotalUserPointsUseCase()
+
+        if (currentPoints < REQUIRED_HINT_POINTS) {
+            throw NotEnoughPointsException()
+        }
+
+        val choices = moviePosterQuestion.movieNameChoices.toMutableList()
+
+        val wrongChoiceToRemove = choices
+            .filter { it != moviePosterQuestion.correctMovieName }
+            .randomOrNull()
+        wrongChoiceToRemove?.let {
+            choices.remove(it)
+            updatePoints(-REQUIRED_HINT_POINTS)
+        }
+
+        return moviePosterQuestion.copy(movieNameChoices = choices)
+    }
+
+    companion object {
+        private const val REQUIRED_HINT_POINTS = 10
+    }
+}
--- domain/src/main/java/com/amsterdam/domain/useCase/profile/GetUserPointsUseCase.kt
+++ domain/src/main/java/com/amsterdam/domain/useCase/profile/GetUserPointsUseCase.kt
@@ -1,12 +1,11 @@
 package com.amsterdam.domain.useCase.profile
 
-import com.amsterdam.domain.repository.GamePointsRepository
-import kotlinx.coroutines.flow.Flow
+import com.amsterdam.domain.repository.GameRepository // Ensure this is the correct GameRepository
 
 class GetUserPointsUseCase(
-    private val gameRepository: GamePointsRepository
+    private val gameRepository: GameRepository
 ) {
-    operator fun invoke(): Flow<Int> {
-        return gameRepository.getPoints()
+    suspend operator fun invoke(): Int {
+        return gameRepository.getUserPoints()
     }
 }
