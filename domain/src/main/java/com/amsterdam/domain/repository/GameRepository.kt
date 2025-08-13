ee8f4dda0f2c7d6584b6e4427f3e66f0a45b9187
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-13 16:26:40 +0300

    feat: add function to get random movies with not null poster

--- domain/src/main/java/com/amsterdam/domain/repository/GameRepository.kt
+++ domain/src/main/java/com/amsterdam/domain/repository/GameRepository.kt
@@ -5,6 +5,7 @@ import com.amsterdam.entity.Movie
 interface GameRepository {
 
     suspend fun getRandomMoviesWithNotNullDate(requiredMoviesNumber: Int): List<Movie>
+    suspend fun getRandomMoviesWithNotNullPoster(requiredMoviesNumber: Int): List<Movie>
 
     suspend fun updatePoints(points: Int)
     suspend fun getUserPoints(): Int
