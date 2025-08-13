6c9e66352c993e91f18413d9e8d6e2716db2ab68
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-13 16:30:47 +0300

    feat: add GuessMovieByPosterGameArgs to viewModelModule

--- app/src/main/java/com/amsterdam/aflami/di/viewModelModule.kt
+++ app/src/main/java/com/amsterdam/aflami/di/viewModelModule.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import com.amsterdam.viewmodel.cast.CastScreenArgs
 import com.amsterdam.viewmodel.categoriesDetails.movies.CategoriesMovieDetailsArgs
 import com.amsterdam.viewmodel.categoriesDetails.tvShow.CategoriesTvShowsDetailsArgs
+import com.amsterdam.viewmodel.guessMovieByPosterGame.GuessMovieByPosterGameArgs
 import com.amsterdam.viewmodel.guessReleseDateGame.GuessReleaseYearGameArgs
 import com.amsterdam.viewmodel.listDetails.ListDetailsArgs
 import com.amsterdam.viewmodel.movieDetails.MovieDetailsArgs
@@ -50,6 +51,11 @@ object ViewModelViewModelScope {
     fun provideListDetailsArgs(
         savedStateHandle: SavedStateHandle
     ): ListDetailsArgs = ListDetailsArgs(savedStateHandle)
+
+    @Provides
+    fun provideGuessMovieByPosterGameArgs(
+        savedStateHandle: SavedStateHandle
+    ): GuessMovieByPosterGameArgs = GuessMovieByPosterGameArgs(savedStateHandle)
 }
 
 @Module
