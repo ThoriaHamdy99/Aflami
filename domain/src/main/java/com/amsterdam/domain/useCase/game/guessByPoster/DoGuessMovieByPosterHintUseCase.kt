da49ad1c158411016d548ebcacf564008b41b8c3
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-13 17:08:44 +0300

    fix: collect total user points in DoGuessMovieByPosterHintUseCase

--- domain/src/main/java/com/amsterdam/domain/useCase/game/guessByPoster/DoGuessMovieByPosterHintUseCase.kt
+++ domain/src/main/java/com/amsterdam/domain/useCase/game/guessByPoster/DoGuessMovieByPosterHintUseCase.kt
@@ -3,7 +3,7 @@ package com.amsterdam.domain.useCase.game.guessByPoster
 import com.amsterdam.domain.exceptions.NotEnoughPointsException
 import com.amsterdam.domain.useCase.game.GetTotalUserPointsUseCase
 import com.amsterdam.domain.useCase.game.UpdateUserGamePointsUseCase
-
+import kotlinx.coroutines.flow.first
 
 class DoGuessMovieByPosterHintUseCase(
     private val getTotalUserPointsUseCase: GetTotalUserPointsUseCase,
@@ -13,7 +13,7 @@ class DoGuessMovieByPosterHintUseCase(
         moviePosterQuestion: MoviePosterQuestion
     ): MoviePosterQuestion {
 
-        val currentPoints = getTotalUserPointsUseCase()
+        val currentPoints = getTotalUserPointsUseCase().first()
 
         if (currentPoints < REQUIRED_HINT_POINTS) {
             throw NotEnoughPointsException()
