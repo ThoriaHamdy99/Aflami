298cfdffaa329413ccf2d6c480a18363c7cfeacb
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-13 16:26:56 +0300

    feat: add getRandomMoviesWithNotNullPoster to GameRepository

--- repository/src/main/java/com/amsterdam/repository/repository/GameRepositoryImpl.kt
+++ repository/src/main/java/com/amsterdam/repository/repository/GameRepositoryImpl.kt
@@ -12,9 +12,15 @@ class GameRepositoryImpl @Inject constructor(
     private val movieRemoteSource: MovieRemoteSource
 ) : GameRepository {
 
-  override suspend fun getRandomMoviesWithNotNullDate(requiredMoviesNumber: Int): List<Movie> {
-   return movieRemoteSource.getRandomMoviesWithNotNullDate(requiredMoviesNumber).toMovieEntityList()
-  }
+    override suspend fun getRandomMoviesWithNotNullDate(requiredMoviesNumber: Int): List<Movie> {
+        return movieRemoteSource.getRandomMoviesWithNotNullDate(requiredMoviesNumber)
+            .toMovieEntityList()
+    }
+
+    override suspend fun getRandomMoviesWithNotNullPoster(requiredMoviesNumber: Int): List<Movie> {
+        return movieRemoteSource.getRandomMoviesWithNotNullPoster(requiredMoviesNumber)
+            .toMovieEntityList()
+    }
 
     override suspend fun updatePoints(points: Int) {
         gameLocalDataSource.upsertPoints(points)
