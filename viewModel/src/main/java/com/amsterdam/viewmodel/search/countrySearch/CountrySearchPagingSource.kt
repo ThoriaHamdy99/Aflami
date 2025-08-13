451c267e6e8266cb461f007ded8858f329ee0184
Ahmed Mahgoub <131989518+ahmedmahgoub0@users.noreply.github.com>
2025-08-13 21:23:41 +0300

    Refactor/inject paging source into search country viewmodel (#481)
    * refactor: inject paging source to country search viewmodel
    
    * refactor: fix broken tests after injecting paging source

---
+++ viewModel/src/main/java/com/amsterdam/viewmodel/search/countrySearch/CountrySearchPagingSource.kt
@@ -0,0 +1,33 @@
+package com.amsterdam.viewmodel.search.countrySearch
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.amsterdam.domain.useCase.search.GetMoviesByCountryUseCase
+import com.amsterdam.entity.Country
+import com.amsterdam.entity.Movie
+import com.amsterdam.paging.BasePagingSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CountrySearchPagingSource @Inject constructor(
+    private val getMoviesByCountryUseCase: GetMoviesByCountryUseCase,
+): BasePagingSource<Movie>() {
+
+    private lateinit var country: Country
+
+    override suspend fun fetch(page: Int): List<Movie> {
+        return getMoviesByCountryUseCase(country, page)
+    }
+
+    fun getMovies(country: Country): Flow<PagingData<Movie>> {
+        this.country = country
+        return Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+                prefetchDistance = PREFETCH_DISTANCE
+            ),
+            pagingSourceFactory = { this }
+        ).flow
+    }
+}
