654b88e92a7de5369c63496f0007143782fe8c20
mohamed <hassanmmd4@gmail.com>
2025-08-13 15:30:13 +0300

    refactor: remove non safe arg from movie details

--- viewModel/src/main/java/com/amsterdam/viewmodel/movieDetails/AddEditScreenArgs.kt
+++ viewModel/src/main/java/com/amsterdam/viewmodel/movieDetails/AddEditScreenArgs.kt
@@ -2,10 +2,6 @@ package com.amsterdam.viewmodel.movieDetails
 
 import androidx.lifecycle.SavedStateHandle
 
-class MovieDetailsArgs(savedStateHandle: SavedStateHandle){
-    val movieId = savedStateHandle.get<Long>(MOVIE_ID_ARGS)
-
-    companion object{
-        const val MOVIE_ID_ARGS = "movieId"
-    }
+interface MovieDetailsArgs{
+    val movieId: Long
 }
