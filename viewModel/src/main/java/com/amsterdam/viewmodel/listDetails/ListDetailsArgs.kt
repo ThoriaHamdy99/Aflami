7c8be88f20f8dad295c9e92dcaf2bd962c4f55ae
mohamed <hassanmmd4@gmail.com>
2025-08-13 15:30:42 +0300

    refactor: remove non safe arg from list details

--- viewModel/src/main/java/com/amsterdam/viewmodel/listDetails/ListDetailsArgs.kt
+++ viewModel/src/main/java/com/amsterdam/viewmodel/listDetails/ListDetailsArgs.kt
@@ -1,13 +1,6 @@
 package com.amsterdam.viewmodel.listDetails
 
-import androidx.lifecycle.SavedStateHandle
-
-class ListDetailsArgs(savedStateHandle: SavedStateHandle) {
-    val listId = savedStateHandle.get<Long>(LIST_ID_ARG) ?: 0
-    val listName = savedStateHandle.get<String>(LIST_NAME_ARG) ?: ""
-
-    companion object {
-        const val LIST_ID_ARG = "listId"
-        const val LIST_NAME_ARG = "listName"
-    }
+interface ListDetailsArgs {
+    val listId: Long
+    val listName: String
 }
