e88b3f87e58b650a1fd59a44dd17d0ba3737862b
mohamed <hassanmmd4@gmail.com>
2025-08-14 19:52:37 +0300

    test: test character game feature usecases

---
+++ domain/src/test/java/com/amsterdam/domain/useCase/game/character/GuessCharacterGameUseCaseTest.kt
@@ -0,0 +1,59 @@
+package com.amsterdam.domain.useCase.game.character
+
+import com.amsterdam.domain.useCase.game.guessByPoster.SubmitGuessMovieByPosterAnswerUseCase
+import com.amsterdam.entity.GameDifficulty
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+class GuessCharacterGameUseCaseTest {
+    private val generateCharacterQuestionsUseCase: GenerateCharacterQuestionsUseCase = mockk()
+    private val submitCharacterAnswerUseCase: SubmitCharacterAnswerUseCase = mockk()
+    private val guessCharacterGameHintUseCase: DoGuessCharacterGameHintUseCase = mockk()
+    private val guessCharacterGameUseCase by lazy {
+        GuessCharacterGameUseCase(
+            generateCharacterQuestionsUseCase,
+            guessCharacterGameHintUseCase,
+            submitCharacterAnswerUseCase
+        )
+    }
+
+    @Test
+    fun `startGame should call generateCharacterQuestionsUseCase`() = runTest {
+        coEvery { generateCharacterQuestionsUseCase(difficultyType) } returns listOf(movieCharacterQuestion)
+
+        val result = guessCharacterGameUseCase.startGame(difficultyType)
+
+        assertThat(result).isEqualTo(listOf(movieCharacterQuestion))
+    }
+
+    @Test
+    fun `giveHint should call guessCharacterGameHintUseCase`() = runTest {
+        coEvery { guessCharacterGameHintUseCase(movieCharacterQuestion) } returns hintedQuestion
+
+        val result = guessCharacterGameUseCase.giveHint(movieCharacterQuestion)
+
+        assertThat(result).isEqualTo(hintedQuestion)
+    }
+
+    @Test
+    fun `checkAnswer should call submitCharacterAnswerUseCase`() = runTest {
+        coEvery { submitCharacterAnswerUseCase(movieCharacterQuestion, "john", difficultyType) } returns answerResult
+
+        val result = guessCharacterGameUseCase.answer(movieCharacterQuestion, "john", difficultyType)
+
+        assertThat(result).isEqualTo(answerResult)
+    }
+
+    private val difficultyType = GameDifficulty.DifficultyType.EASY
+    private val movieCharacterQuestion = GenerateCharacterQuestionsUseCase.CharacterDataQuestion(
+        questionAsPosterUrl = "character",
+        choices = listOf("john", "man", "woman"),
+        correctAnswer = "john",
+        questionTimeSeconds = 30
+    )
+    private val hintedQuestion = movieCharacterQuestion.copy(choices = listOf("john"))
+    private val answerResult = SubmitCharacterAnswerUseCase.AnswerResult(true, 10)
+}
