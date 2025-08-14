0260a98d6cffe69b3cdd791115d56fb35367966b
Mona <92681072+monaayman123@users.noreply.github.com>
2025-08-14 15:08:04 +0300

    Fix/modify list placeholder image to match the design image (#480)
    * refactor: add transparency to images
    
    * fix: change default value of imageAlpha from 0f to be 1f

--- ui/src/main/java/com/amsterdam/ui/components/NoDataContainer.kt
+++ ui/src/main/java/com/amsterdam/ui/components/NoDataContainer.kt
@@ -1,6 +1,7 @@
 package com.amsterdam.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,16 +29,19 @@ fun NoDataContainer(
     modifier: Modifier = Modifier,
     title: String = "",
     description: String = "",
+    imageAlpha: Float = 1f,
 ) {
     Column(
         modifier =
             modifier
                 .fillMaxWidth()
+                .background(AppTheme.color.surface)
                 .padding(start = 24.dp, end = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
         Image(
+            modifier = Modifier.alpha(imageAlpha),
             painter = imageRes,
             contentDescription = null,
         )
