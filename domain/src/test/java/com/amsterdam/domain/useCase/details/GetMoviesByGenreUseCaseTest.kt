1c846cea487c4d3dceb312ec4fa4d189e08ac66c
Mona <monaayman584@gmail.com>
2025-08-14 12:25:39 +0300

    test: move variable definations outside function and initialize repo and use case by lazy

--- domain/src/test/java/com/amsterdam/domain/useCase/details/GetMoviesByGenreUseCaseTest.kt
+++ domain/src/test/java/com/amsterdam/domain/useCase/details/GetMoviesByGenreUseCaseTest.kt
@@ -8,31 +8,33 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 
 class GetMoviesByGenreUseCaseTest {
-    private lateinit var movieRepository: MovieRepository
-    private lateinit var getMoviesByGenreUseCase: GetMoviesByGenreUseCase
-
-
-    @BeforeEach
-    fun setUp() {
-        movieRepository = mockk()
-        getMoviesByGenreUseCase = GetMoviesByGenreUseCase(movieRepository)
+    private val movieRepository: MovieRepository = mockk()
+    val getMoviesByGenreUseCase by lazy {
+        GetMoviesByGenreUseCase(movieRepository)
     }
 
     @Test
     fun `invoke should call getMoviesByGenre from movieRepository`() = runTest {
-        val selectedGenre = MovieGenre.ACTION
-        val page = 1
-        val movies = fakeMovieListWithCategories
+
         coEvery { movieRepository.getMoviesByGenre(selectedGenre, page) } returns movies
 
         val result = getMoviesByGenreUseCase(selectedGenre, page)
 
         assertThat(result).isEqualTo(movies)
-        coVerify { movieRepository.getMoviesByGenre(selectedGenre, page) }
     }
+
+    @Test
+    fun `should call getMoviesByGenre from movieRepository`() = runTest {
+        coEvery { movieRepository.getMoviesByGenre(selectedGenre, page) } returns mockk()
+        getMoviesByGenreUseCase(selectedGenre, page)
+        coVerify(exactly = 1) { movieRepository.getMoviesByGenre(selectedGenre, page) }
+    }
+
+    val selectedGenre = MovieGenre.ACTION
+    val page = 1
+    val movies = fakeMovieListWithCategories
 }
