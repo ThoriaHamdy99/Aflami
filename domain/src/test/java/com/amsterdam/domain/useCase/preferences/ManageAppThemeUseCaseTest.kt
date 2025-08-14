5d6496a6ee4c4968b78ae7dd81303d5bb6876572
mohamed <hassanmmd4@gmail.com>
2025-08-14 14:17:17 +0300

    test: fix broken test for theme

--- domain/src/test/java/com/amsterdam/domain/useCase/preferences/ManageAppThemeUseCaseTest.kt
+++ domain/src/test/java/com/amsterdam/domain/useCase/preferences/ManageAppThemeUseCaseTest.kt
@@ -1,52 +1,48 @@
+package com.amsterdam.domain.useCase.preferences
+
 import com.amsterdam.domain.repository.AppPreferencesRepository
-import com.amsterdam.domain.useCase.preferences.ManageAppThemeUseCase
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
 
 class ManageAppThemeUseCaseTest {
 
     private lateinit var preferencesRepository: AppPreferencesRepository
     private lateinit var manageAppThemeUseCase: ManageAppThemeUseCase
 
-    @Before
-    fun setup() {
+    @BeforeEach
+    fun setUp() {
         preferencesRepository = mockk(relaxed = true)
         manageAppThemeUseCase = ManageAppThemeUseCase(preferencesRepository)
     }
 
     @Test
     fun `setAppTheme should call repository with correct value`() = runTest {
-        // Given
         val isDark = true
         coEvery { preferencesRepository.setAppTheme(isDark) } returns Unit
 
-        // When
         manageAppThemeUseCase.setAppTheme(isDark)
 
-        // Then
         coVerify { preferencesRepository.setAppTheme(isDark) }
     }
 
     @Test
     fun `getAppTheme should return expected theme`() = runTest {
-        // Given
         val expected = true
-       coEvery{ preferencesRepository.getAppTheme() } returns flowOf(expected)
+        coEvery{ preferencesRepository.getAppTheme() } returns flowOf(expected)
 
-        // When
         val themeFlow = manageAppThemeUseCase.getAppTheme()
         var actual: Boolean? = null
         themeFlow.collect { value ->
             actual = value
         }
 
-        // Then
         assertThat(actual).isEqualTo(expected)
         coVerify { preferencesRepository.getAppTheme() }
     }
