53de679e0b3ca5e2ffd29aaa946b9da3a6a137c7
mohamed <hassanmmd4@gmail.com>
2025-08-14 14:16:48 +0300

    test: add test cases for restriction level

---
+++ domain/src/test/java/com/amsterdam/domain/useCase/preferences/ManageRestrictionLevelUseCaseTest.kt
@@ -0,0 +1,49 @@
+package com.amsterdam.domain.useCase.preferences
+
+import com.amsterdam.domain.repository.AppPreferencesRepository
+import com.amsterdam.domain.utils.RestrictionLevel
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ManageRestrictionLevelUseCaseTest {
+
+    private lateinit var preferencesRepository: AppPreferencesRepository
+    private lateinit var manageRestrictionLevelUseCase: ManageRestrictionLevelUseCase
+
+    @BeforeEach
+    fun setUp() {
+        preferencesRepository = mockk(relaxed = true)
+        manageRestrictionLevelUseCase = ManageRestrictionLevelUseCase(preferencesRepository)
+    }
+
+    @Test
+    fun `setRestrictionLevel should call repository with correct value`() = runTest {
+        val restrictionLevel = RestrictionLevel.STRICT
+        coEvery { preferencesRepository.setRestrictionLevel(restrictionLevel) } returns Unit
+
+        manageRestrictionLevelUseCase.setRestrictionLevel(restrictionLevel)
+
+        coVerify { preferencesRepository.setRestrictionLevel(restrictionLevel) }
+    }
+
+    @Test
+    fun `getRestrictionLevel should return expected restriction level`() = runTest {
+        val expected = RestrictionLevel.MODERATE
+        coEvery{ preferencesRepository.getRestrictionLevel() } returns flowOf(expected)
+
+        val restrictionLevelFlow = manageRestrictionLevelUseCase.getRestrictionLevel()
+        lateinit var actual: RestrictionLevel
+        restrictionLevelFlow.collect { value ->
+            actual = value
+        }
+
+        assertThat(actual).isEqualTo(expected)
+        coVerify { preferencesRepository.getRestrictionLevel() }
+    }
+}
