5161b406b42e0919a73dbaaa48692000607aad49
Mona <monaayman584@gmail.com>
2025-08-14 12:26:00 +0300

    test: fix broken test

--- domain/src/test/java/com/amsterdam/domain/useCase/profile/GetUserPointsUseCaseTest.kt
+++ domain/src/test/java/com/amsterdam/domain/useCase/profile/GetUserPointsUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.amsterdam.domain.useCase.profile
 
+import com.amsterdam.domain.repository.GameRepository
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -12,21 +13,21 @@ import org.junit.jupiter.api.Test
 
 class GetUserPointsUseCaseTest {
 
-    private val gamePointsRepository = mockk<GamePointsRepository>()
-    private val getUserPointsUseCase = GetUserPointsUseCase(gamePointsRepository)
+    private val gameRepository = mockk<GameRepository>()
+    private val getUserPointsUseCase = GetUserPointsUseCase(gameRepository)
 
     @Test
     fun `should call getPoints from repository`() = runTest {
-        coEvery { gamePointsRepository.getPoints() } returns flowOf(40)
+        coEvery { gameRepository.getUserPoints() } returns flowOf(40)
 
         getUserPointsUseCase.invoke()
 
-        coVerify(exactly = 1) { gamePointsRepository.getPoints() }
+        coVerify(exactly = 1) { gameRepository.getUserPoints() }
     }
 
     @Test
     fun `should return the same points flow as repository`() = runTest {
-        every { gamePointsRepository.getPoints() } returns flowOf(40)
+        every { gameRepository.getUserPoints() } returns flowOf(40)
 
         assertThat(getUserPointsUseCase().first()).isEqualTo(40)
     }
