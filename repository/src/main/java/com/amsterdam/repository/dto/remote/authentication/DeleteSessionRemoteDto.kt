bc1d8a3ab5dbc79b5148e875acde2d8368c2990a
saraashraf11 <saraashraf1822004@gmail.com>
2025-08-15 05:26:54 +0300

    refactor: delete unused files

--- repository/src/main/java/com/amsterdam/repository/dto/remote/authentication/DeleteSessionRemoteDto.kt
+++
@@ -1,9 +0,0 @@
-package com.amsterdam.repository.dto.remote.authentication
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DeleteSessionRemoteDto(
-    @SerialName("session_id") val sessionId: String
-)
