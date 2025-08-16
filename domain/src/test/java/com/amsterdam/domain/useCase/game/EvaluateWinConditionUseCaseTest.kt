ed259ab191c1d8aec0b144c0759f5ca2a2ac1aff
Ahmed Mahgoub <aed7862@gmail.com>
2025-08-17 00:42:10 +0300

    refactor: add test case to verify that function in repository called

--- domain/src/test/java/com/amsterdam/domain/useCase/game/EvaluateWinConditionUseCaseTest.kt
+++ domain/src/test/java/com/amsterdam/domain/useCase/game/EvaluateWinConditionUseCaseTest.kt
@@ -4,12 +4,22 @@ import com.amsterdam.domain.repository.GameRepository
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 
 class EvaluateWinConditionUseCaseTest {
 
     private val gameRepository: GameRepository = mockk()
     private val evaluateWinConditionUseCase = EvaluateWinConditionUseCase(gameRepository)
+
+    @Test
+    fun `should call getCollectedPoints when collected evaluateWinConditionUseCase called`() {
+        every { gameRepository.getCollectedPoints(any()) } returns 0
+
+        evaluateWinConditionUseCase(1L)
+
+        verify(exactly = 1) { gameRepository.getCollectedPoints(any()) }
+    }
 
     @Test
     fun `evaluateWinConditionUseCase should return true when collected points greater than 0`() {
