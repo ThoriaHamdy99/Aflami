794649d922d02bbdcf80458b86edb9bba564ae00
saraashraf11 <saraashraf1822004@gmail.com>
2025-08-16 18:13:12 +0300

    test: add TvShowWatchHistoryMapperTest

---
+++ repository/src/test/java/com/amsterdam/repository/mapper/TvShowWatchHistoryMapperTest.kt
@@ -0,0 +1,47 @@
+package com.amsterdam.repository.mapper
+
+import com.amsterdam.entity.TvShowWatchHistory
+import com.amsterdam.repository.dto.local.TvShowLocalDto
+import com.amsterdam.repository.dto.local.TvShowWatchHistoryDto
+import com.google.common.truth.Truth.assertThat
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class TvShowWatchHistoryMapperTest {
+    private val tvShowLocalDto = TvShowLocalDto(
+        tvShowId = 201L,
+        storedLanguage = "en",
+        name = "Test TV Show",
+        description = "An overview.",
+        poster = "/poster.jpg",
+        airDate = LocalDate.parse("2023-10-26"),
+        rating = 8.5f,
+        popularity = 1500.0,
+        seasonCount = 5,
+        originCountry = "US"
+    )
+
+    private val tvShowWatchHistoryDto = TvShowWatchHistoryDto(
+        tvShowId = 201L,
+        watchedDate = Instant.parse("2024-01-20T12:00:00Z")
+    )
+
+    @Nested
+    inner class ToEntityTest {
+        @Test
+        fun `toEntity should map DTOs to TvShowWatchHistory entity`() {
+            val expectedTvShowEntity = tvShowLocalDto.toEntity()
+
+            val result = tvShowWatchHistoryDto.toEntity(tvShowLocalDto)
+
+            assertThat(result).isEqualTo(
+                TvShowWatchHistory(
+                    tvShow = expectedTvShowEntity,
+                    lastWatchedTime = tvShowWatchHistoryDto.watchedDate
+                )
+            )
+        }
+    }
+}
