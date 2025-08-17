5d571a11d469b635c3bed0ceeaa3d9e8590e1fc0
Thoraya <thoriahamdy99@gmail.com>
2025-08-18 01:41:44 +0300

    refactor: use LogManager instead of logger in responseCall.

--- remoteDatasource/src/main/java/com/amsterdam/remotedatasource/utils/apiHandler/responseCall.kt
+++ remoteDatasource/src/main/java/com/amsterdam/remotedatasource/utils/apiHandler/responseCall.kt
@@ -10,7 +10,7 @@ import com.amsterdam.domain.exceptions.NoInternetException
 import com.amsterdam.domain.exceptions.ServerErrorException
 import com.amsterdam.domain.exceptions.UnknownException
 import com.amsterdam.domain.exceptions.VerificationRequiredException
-import com.amsterdam.domain.logger.Logger
+import com.amsterdam.domain.logger.LogManager
 import kotlinx.serialization.SerializationException
 import retrofit2.HttpException
 import java.net.ConnectException
@@ -18,77 +18,106 @@ import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
 suspend inline fun <reified T> responseCall(
-    logger: Logger,
     crossinline execute: suspend () -> T,
     onHttpError: (String) -> Int? = { null },
-): T =
-    try {
+): T {
+    val tag = "responseCall"
+    return try {
         val result = execute()
-        logger.debug("Result: $result")
+        LogManager.debug(tag, "Result: $result")
         result
     } catch (e: HttpException) {
         throw if (e.code() == 401) {
             val serverResponse = e.response()?.errorBody()?.string() ?: throw UnknownException()
-            mapHttpCodeToDomainException(onHttpError(serverResponse), logger, e)
+            mapHttpCodeToDomainException(onHttpError(serverResponse), tag, e)
         } else {
-            logger.error("HttpException", e)
+            LogManager.error(tag, "HttpException", e)
             ServerErrorException()
         }
     } catch (e: ConnectException) {
-        logger.error("ConnectException: No internet connection", e)
+        LogManager.error(tag, "ConnectException: No internet connection", e)
         throw NoInternetException()
     } catch (e: SerializationException) {
-        logger.error("SerializationException: Serialization error", e)
+        LogManager.error(tag, "SerializationException: Serialization error", e)
         throw ServerErrorException()
     } catch (e: UnknownHostException) {
-        logger.error("UnknownHostException: No internet ", e)
+        LogManager.error(tag, "UnknownHostException: No internet ", e)
         throw NoInternetException()
     } catch (e: SocketTimeoutException) {
-        logger.error("SocketTimeoutException: No internet", e)
+        LogManager.error(tag, "SocketTimeoutException: No internet", e)
         throw NetworkException()
     } catch (e: Exception) {
-        logger.errorWithCrashlytics("Exception: Unknown error", e)
+        LogManager.errorWithCrashlytics(tag, "Exception: Unknown error", e)
         throw NetworkException()
     }
+}
 
 fun mapHttpCodeToDomainException(
     code: Int?,
-    logger: Logger,
+    tag: String,
     exception: HttpException
 ): NetworkException =
     when (code) {
         3, 14, 30 -> {
-            logger.error("HttpException: Invalid credentials", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Invalid credentials",
+                exception
+            )
             InvalidCredentialsException()
         }
 
         7, 10, 16, 17, 33, 35, 36 -> {
-            logger.error("HttpException: Invalid session", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Invalid session",
+                exception
+            )
             InvalidSessionException()
         }
 
         31 -> {
-            logger.error("HttpException: Account disabled", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Account disabled",
+                exception
+            )
             AccountDisabledException()
         }
 
         32 -> {
-            logger.error("HttpException: Verification required", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Verification required",
+                exception
+            )
             VerificationRequiredException()
         }
 
         38, 39 -> {
-            logger.error("HttpException: Access denied", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Access denied",
+                exception
+            )
             AccessDeniedException()
         }
 
         45 -> {
-            logger.error("HttpException: Access restricted", exception)
+            LogManager.error(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Access restricted",
+                exception
+            )
             AccessRestrictedException()
         }
 
         else -> {
-            logger.errorWithCrashlytics("HttpException: Unknown error", exception)
+            LogManager.errorWithCrashlytics(
+                tag + "mapHttpCodeToDomainException",
+                "HttpException: Unknown error",
+                exception
+            )
             NetworkException()
         }
     }
