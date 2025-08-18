f88b875a5451840e5462cb814cb12de1ca34261a
Eslam <eslam.mohamed.551999@gmail.com>
2025-08-18 22:04:43 +0300

    fix: remove extra background around search bar for better transition

--- ui/src/main/java/com/amsterdam/ui/screens/search/keywordSearch/sections/SearchScreenHeaderSection.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/search/keywordSearch/sections/SearchScreenHeaderSection.kt
@@ -1,7 +1,6 @@
 package com.amsterdam.ui.screens.search.keywordSearch.sections
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,10 +13,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import com.amsterdam.ui.R
 import com.amsterdam.designsystem.components.TabsLayout
 import com.amsterdam.designsystem.components.TextField
-import com.amsterdam.designsystem.theme.AppTheme
+import com.amsterdam.ui.R
 import com.amsterdam.ui.components.appBar.DefaultAppBar
 import com.amsterdam.viewmodel.shared.TabOption
 
@@ -41,14 +39,11 @@ fun SearchScreenHeaderSection(
         )
 
         TextField(
-            modifier =
-                Modifier
-                    .background(color = AppTheme.color.surface)
-                    .padding(top = 8.dp),
+            modifier = Modifier.padding(top = 8.dp),
             text = keyword,
             onValueChange = { onKeywordValuedChanged(it) },
             hintText = stringResource(R.string.search_hint),
-            trailingIcon = com.amsterdam.ui.R.drawable.ic_filter_vertical,
+            trailingIcon = R.drawable.ic_filter_vertical,
             onTrailingClick = onFilterButtonClicked,
             isTrailingClickEnabled = keyword.isNotBlank(),
             isError = keyword.length > 100,
