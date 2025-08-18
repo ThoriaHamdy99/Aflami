614f643323205032f3fc0ab65115c12cf0897a33
Eslam <eslam.mohamed.551999@gmail.com>
2025-08-19 00:36:40 +0300

    fix: fix explore composable padding

--- ui/src/main/java/com/amsterdam/ui/screens/search/countrySearch/SearchByCountryScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/search/countrySearch/SearchByCountryScreen.kt
@@ -145,7 +145,7 @@ private fun SearchByCountryContent(
                                     !state.isLoading &&
                                     state.selectedCountryIsoCode.isNotBlank() -> NoMoviesFound()
 
-                            movies.itemCount == 0 -> ExploreCountries()
+                            movies.itemCount == 0 -> ExploreCountries(Modifier.padding(horizontal = 16.dp))
 
                             else -> {}
                         }
