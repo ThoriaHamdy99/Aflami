6821261099136b9c231f1619a95268f18f6b18c9
Thoraya <thoriahamdy99@gmail.com>
2025-08-18 23:23:41 +0300

    test: update AuthenticationRepositoryImplTest test functions

--- repository/src/test/java/com/amsterdam/repository/repository/AuthenticationRepositoryImplTest.kt
+++ repository/src/test/java/com/amsterdam/repository/repository/AuthenticationRepositoryImplTest.kt
@@ -40,22 +40,34 @@ class AuthenticationRepositoryImplTest {
 
     @Test
     fun `loginWithPassword should throw exception on remote failure`() = runTest {
-        val expectedException = RuntimeException("Remote login failed")
         coEvery {
             authenticationRemoteDataSource.loginWithPassword(
                 any(),
                 any()
             )
-        } throws expectedException
+        } throws RuntimeException()
 
-        val thrownException = assertThrows<RuntimeException> {
+        assertThrows<RuntimeException> {
             repository.loginWithPassword("user", "pass")
         }
-
-        assertThat(thrownException).isEqualTo(expectedException)
-        coVerify(exactly = 0) { authenticationLocalDataSource.cacheSessionId(any()) }
-        coVerify(exactly = 0) { authenticationLocalDataSource.setSessionType(any()) }
     }
+
+    @Test
+    fun `loginWithPassword should not call cacheSessionId or setSessionType when login fails`() =
+        runTest {
+            coEvery {
+                authenticationRemoteDataSource.loginWithPassword(
+                    any(),
+                    any()
+                )
+            } throws RuntimeException()
+            assertThrows<RuntimeException> {
+                repository.loginWithPassword("user", "pass")
+            }
+
+            coVerify(exactly = 0) { authenticationLocalDataSource.cacheSessionId(any()) }
+            coVerify(exactly = 0) { authenticationLocalDataSource.setSessionType(any()) }
+        }
 
     @Test
     fun `setSessionType should call local data source`() = runTest {
@@ -69,14 +81,11 @@ class AuthenticationRepositoryImplTest {
 
     @Test
     fun `setSessionType should rethrow exception if local source fails`() = runTest {
-        val expectedException = RuntimeException("Failed to write session type")
-        coEvery { authenticationLocalDataSource.setSessionType(any()) } throws expectedException
+        coEvery { authenticationLocalDataSource.setSessionType(any()) } throws RuntimeException()
 
-        val thrownException = assertThrows<RuntimeException> {
+        assertThrows<RuntimeException> {
             repository.setSessionType(SessionType.GUEST)
         }
-
-        assertThat(thrownException).isEqualTo(expectedException)
     }
 
     @Test
@@ -90,15 +99,21 @@ class AuthenticationRepositoryImplTest {
     }
 
     @Test
-    fun `getSessionType should rethrow exception if local source fails`() = runTest {
-        val expectedException = RuntimeException("Failed to read session type")
-        coEvery { authenticationLocalDataSource.getSessionType() } throws expectedException
+    fun `getSessionType should call getSessionType in data source once`() = runTest {
+        coEvery { authenticationLocalDataSource.getSessionType() } returns "GUEST"
 
-        val thrownException = assertThrows<RuntimeException> {
+        repository.getSessionType()
+
+        coVerify(exactly = 1) { authenticationLocalDataSource.getSessionType() }
+    }
+
+    @Test
+    fun `getSessionType should rethrow exception if local source fails`() = runTest {
+        coEvery { authenticationLocalDataSource.getSessionType() } throws RuntimeException()
+
+        assertThrows<RuntimeException> {
             repository.getSessionType()
         }
-
-        assertThat(thrownException).isEqualTo(expectedException)
     }
 
     @Test
@@ -116,15 +131,22 @@ class AuthenticationRepositoryImplTest {
 
     @Test
     fun `logout should throw exception if clearing session fails and stop execution`() = runTest {
-        val expectedException = RuntimeException("Failed to clear session ID")
-        coEvery { authenticationLocalDataSource.clearCachedSessionId() } throws expectedException
+        coEvery { authenticationLocalDataSource.clearCachedSessionId() } throws RuntimeException()
 
-        val thrownException = assertThrows<RuntimeException> {
+        assertThrows<RuntimeException> {
             repository.logout()
         }
-
-        assertThat(thrownException).isEqualTo(expectedException)
-        coVerify(exactly = 0) { authenticationLocalDataSource.setSessionType(any()) }
-        coVerify(exactly = 0) { profileLocalDataSource.deleteAccountDetails() }
     }
+
+    @Test
+    fun `logout should should not call setSessionType or deleteAccountDetails when clearing session fails`() =
+        runTest {
+            coEvery { authenticationLocalDataSource.clearCachedSessionId() } throws RuntimeException()
+            assertThrows<RuntimeException> {
+                repository.logout()
+            }
+
+            coVerify(exactly = 0) { authenticationLocalDataSource.setSessionType(any()) }
+            coVerify(exactly = 0) { profileLocalDataSource.deleteAccountDetails() }
+        }
 }
