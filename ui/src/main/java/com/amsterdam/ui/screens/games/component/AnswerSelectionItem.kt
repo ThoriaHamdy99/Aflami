57c87556502a5630f36746570945a0b16818ab44
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-08-18 18:23:43 +0100

    fix: enable click on RadioButton in AnswerSelectionItem

--- ui/src/main/java/com/amsterdam/ui/screens/games/component/AnswerSelectionItem.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/games/component/AnswerSelectionItem.kt
@@ -16,20 +16,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.amsterdam.ui.R
 import com.amsterdam.designsystem.components.Icon
+import com.amsterdam.designsystem.components.Text
 import com.amsterdam.designsystem.components.buttons.RadioButton
 import com.amsterdam.designsystem.components.buttons.RadioState
-import com.amsterdam.designsystem.components.Text
 import com.amsterdam.designsystem.theme.AflamiTheme
 import com.amsterdam.designsystem.theme.AppTheme
 import com.amsterdam.designsystem.utils.ThemeAndLocalePreviews
+import com.amsterdam.ui.R
 
 @Composable
 fun AnswerSelectionItem(
     modifier: Modifier = Modifier,
     text: String = "",
-    isClickable : Boolean = true,
+    isClickable: Boolean = true,
     status: AnswerStatus = AnswerStatus.Unselected,
     onClick: () -> Unit = {}
 ) {
@@ -37,19 +37,19 @@ fun AnswerSelectionItem(
 
     Row(
         modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = status.backgroundColor(),
-                shape = shape,
-            )
-            .border(
-                color = status.borderColor(),
-                width = 1.dp,
-                shape = shape,
-            )
-            .clip(shape)
-            .clickable(enabled = isClickable,onClick = onClick)
-            .padding(vertical = 16.dp, horizontal = 12.dp),
+                .fillMaxWidth()
+                .background(
+                    color = status.backgroundColor(),
+                    shape = shape,
+                )
+                .border(
+                    color = status.borderColor(),
+                    width = 1.dp,
+                    shape = shape,
+                )
+                .clip(shape)
+                .clickable(enabled = isClickable, onClick = onClick)
+                .padding(vertical = 16.dp, horizontal = 12.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
 
@@ -58,13 +58,14 @@ fun AnswerSelectionItem(
             color = AppTheme.color.body,
             style = AppTheme.textStyle.label.large,
             modifier = Modifier
-                .weight(1f)
-                .padding(start = 8.dp)
+                    .weight(1f)
+                    .padding(start = 8.dp)
         )
         when (status) {
             AnswerStatus.Unselected -> {
                 RadioButton(
                     state = RadioState.Default,
+                    onClick = onClick
                 )
             }
 
