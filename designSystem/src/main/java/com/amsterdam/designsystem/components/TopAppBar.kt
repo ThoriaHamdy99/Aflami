b95713aab842c56426b3d92e4a66b86956d3d3ff
Thoraya <thoriahamdy99@gmail.com>
2025-08-19 02:21:20 +0300

    fix: resolve padding problem between title and subTitle in top app bar.

--- designSystem/src/main/java/com/amsterdam/designsystem/components/TopAppBar.kt
+++ designSystem/src/main/java/com/amsterdam/designsystem/components/TopAppBar.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,9 +45,16 @@ fun TopAppBar(
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.height(IntrinsicSize.Min)
         ) {
             leadingIcon?.invoke()
-            Column {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 2.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.Start
+            ) {
                 title?.invoke()
                 subTitle?.invoke()
             }
